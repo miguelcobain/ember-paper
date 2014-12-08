e73e31dc2bf5a0f044599379a29c02aec72c263e
Alex LaFroscia <alex@lafroscia.com>
2014-12-07 21:12:05 -0500

    Add dummy app

---
+++ tests/dummy/app/controllers/checkboxes.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+export default Ember.Controller.extend({
+
+  value: false
+
+});
+
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('checkboxes');
 });
 
 export default Router;
