0390364aa83d00ebb6d18b0757584bdcec5fb4ee
Alex LaFroscia <alex@lafroscia.com>
2014-12-07 21:53:56 -0500

    Add dummy page for toggles

---
+++ tests/dummy/app/controllers/toggles.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+export default Ember.Controller.extend({
+
+  booleanProp1: true,
+  booleanProp2: true
+
+});
+
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('checkboxes');
+  this.route('toggles');
 });
 
 export default Router;
