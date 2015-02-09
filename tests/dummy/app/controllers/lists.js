e9a24dea4bba2bd7306305770ae80debddc43d16
Miguel Andrade <jmandrade0@gmail.com>
2015-02-09 19:15:30 +0000

    fix tomster path

--- tests/dummy/app/controllers/lists.js
+++ tests/dummy/app/controllers/lists.js
@@ -2,27 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   listData: [{
-    face: '/tomster.png',
+    face: 'tomster.png',
     who: 'tomster',
     what: 'a message for you',
     notes: 'this is a message for you about ember'
   },{
-    face: '/tomster.png',
+    face: 'tomster.png',
     who: 'tomster',
     what: 'a message for you',
     notes: 'this is a message for you about ember'
   },{
-    face: '/tomster.png',
+    face: 'tomster.png',
     who: 'tomster',
     what: 'a message for you',
     notes: 'this is a message for you about ember'
   },{
-    face: '/tomster.png',
+    face: 'tomster.png',
     who: 'tomster',
     what: 'a message for you',
     notes: 'this is a message for you about ember'
   },{
-    face: '/tomster.png',
+    face: 'tomster.png',
     who: 'tomster',
     what: 'a message for you',
     notes: 'this is a message for you about ember'
