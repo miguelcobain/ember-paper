cdfc16ff7e801621a65cd19229d64bab224798fb
Miguel Andrade <jmandrade0@gmail.com>
2015-02-11 10:38:50 +0000

    paper-button refactor. Closes #34 and #19.

--- addon/components/paper-button.js
+++ addon/components/paper-button.js
@@ -1,53 +1,26 @@
 import Ember from 'ember';
 import BaseFocusable from './base-focusable';
-import ShadowMixin from '../mixins/shadow-mixin';
 import RippleMixin from '../mixins/ripple-mixin';
 
-export default BaseFocusable.extend(ShadowMixin,RippleMixin,{
+export default BaseFocusable.extend(RippleMixin,{
   attributeBindings: ['target', 'action'],
-  tagName:'button',
-  classNames:['md-button','md-default-theme'],
+  tagName: 'button',
+  classNames: ['md-button','md-default-theme'],
+  classNameBindings: ['raised:md-raised'],
 
   /* RippleMixin overrides */
   center: false,
   dimBackground: true,
 
-  /* ShadowMixin properties */
-  z:1,
-
-  defaultZ:1,
-  activeZ:2,
-  disabledZ:0,
-
-  /*
-   * Function that handles button state changes.
-   * Changes z and sends action.
-   */
-  stateDidChange:Ember.observer('active','focus','disabled','hover',function(){
-    var active = this.get('active'),
-      disabled = this.get('disabled'),
-      hover = this.get('hover'),
-      focus = this.get('focus');
-    if (active || focus || hover) {
-      this.set('z',this.get('activeZ'));
-    } else if (disabled) {
-      this.set('z',this.get('disabledZ'));
-    } else {
-      this.set('z',this.get('defaultZ'));
-    }
-  }),
-
-  click:function(){
+  click: function(){
     var target = this.get('target');
 
     if (target) {
       this.get('target').send(this.get('action'));
-
-      if (typeof this.get('bubbles') !== 'undefined' && !this.get('bubbles')) {
-        return;
-      }
+    } else {
+      this.sendAction();
     }
 
-    this.sendAction();
+    return typeof this.get('bubbles') == 'undefined' || this.get('bubbles') == true;
   }
 });
