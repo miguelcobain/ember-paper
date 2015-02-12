f7e6cd692a55e34d115d952ea093cf5f9ccc850d
Miguel <jmandrade0@gmail.com>
2015-02-12 17:07:13 +0000

    fix boolean flex attribute

--- addon/components/paper-content.js
+++ addon/components/paper-content.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName:'md-content',
   classNames:['md-content'],
-  attributeBindings:['flex','flex-layout:layout']
+  attributeBindings:['flexAttr:flex','flex-layout:layout'],
+  /*
+   * Not binding boolean values in Ember 1.8.1?
+   * https://github.com/emberjs/ember.js/issues/9595
+   */
+  flexAttr:function(){
+    return this.get('flex') ? 'flex' : null;
+  }.property('flex')
 });
