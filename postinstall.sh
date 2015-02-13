dfdfd0c1a99fd2d454ba2fb22e37075f6cb325c4
Samy Alzhrani <samy@sadeem.net>
2015-02-13 18:07:56 +0300

    Install it before using it

--- postinstall.sh
+++ postinstall.sh
@@ -12,5 +12,8 @@ npm install node-sass
 echo "compiling CSS to /vendor with node-sass"
 ./node_modules/.bin/node-sass ./addon/styles/scss/main.scss ./vendor/ember-paper.css
 
+echo "install autoprefixer"
+npm install autoprefixer
+
 echo "autoprefixing"
 ./node_modules/autoprefixer/autoprefixer -b "last 2 versions" ./vendor/ember-paper.css
