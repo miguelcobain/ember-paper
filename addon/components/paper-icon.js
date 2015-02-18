be3883a410340eda8d25eec907de959c79010b84
Mhretab Kidane <mhretabk@gmail.com>
2015-02-18 16:56:26 +0100

    added class bindings for icon sizes and spinners

--- addon/components/paper-icon.js
+++ addon/components/paper-icon.js
@@ -4,7 +4,37 @@ export default Ember.Component.extend({
   tagName: 'md-icon',
   classNames: ['paper-icon'],
   classNameBindings: ['iconClass'],
+
+  spin: false,
+  reverseSpin: false,
+
   iconClass: function(){
-    return 'ic-'+this.get('icon');
+    var iconClasses = 'ic-'+this.get('icon');
+
+    if(this.get('spin')){
+      iconClasses +=' md-spin';
+    } else if (this.get('reverseSpin')){
+      iconClasses +=' md-spin-reverse';
+    }
+
+    switch(this.get('size')){
+      case 'lg':
+        iconClasses += ' md-lg';
+        break;
+      case 2:
+        iconClasses += ' md-2x';
+        break;
+      case 3:
+        iconClasses += ' md-3x';
+        break;
+      case 4:
+        iconClasses += ' md-4x';
+        break;
+      case 5:
+        iconClasses += ' md-5x';
+        break;
+    }
+
+    return iconClasses;
   }.property('icon')
 });
