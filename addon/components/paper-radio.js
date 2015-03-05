b29e2e6903121ba5e1998cde31551626ae6bcc26
Miguel <jmandrade0@gmail.com>
2015-03-05 16:22:55 +0000

    add paper-radio tests. code styling.

--- addon/components/paper-radio.js
+++ addon/components/paper-radio.js
@@ -2,28 +2,29 @@ import Ember from 'ember';
 import BaseFocusable from './base-focusable';
 import RippleMixin from '../mixins/ripple-mixin';
 
-export default BaseFocusable.extend(RippleMixin,{
-  tagName:'md-radio-button',
-  classNames:['md-radio-button','md-default-theme'],
-  classNameBindings:['checked:md-checked'],
-  toggle:false,
+export default BaseFocusable.extend(RippleMixin, {
+  tagName: 'md-radio-button',
+  classNames: ['md-radio-button', 'md-default-theme'],
+  classNameBindings: ['checked:md-checked'],
+  toggle: false,
+  selected: null,
 
   center: true,
-  rippleContainerSelector:'.md-container',
+  rippleContainerSelector: '.md-container',
 
   checked: function() {
     return this.get('value') === this.get('selected');
   }.property('value', 'selected'),
 
-  checkedDidChange: Ember.observer('checked',function() {
-    if(this.get('checked')){
+  checkedDidChange: Ember.observer('checked', function() {
+    if (this.get('checked')) {
       this.set('selected', this.get('value'));
     }
   }),
 
-  click:function(){
-    if(this.toggle){
-      this.set('selected', this.get('checked')?null:this.get('value'));
+  click: function() {
+    if (this.get('toggle')) {
+      this.set('selected', this.get('checked') ? null : this.get('value'));
     } else {
       this.set('selected', this.get('value'));
     }
--- tests/unit/components/paper-radio-test.js
+++ tests/unit/components/paper-radio-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import {
   moduleForComponent,
   test
@@ -16,6 +17,65 @@ test('it renders', function(assert) {
   assert.equal(component._state, 'preRender');
 
   // appends the component to the page
-  this.append();
+  this.render();
   assert.equal(component._state, 'inDOM');
+});
+
+test('should set checked css class', function(assert) {
+  var component = this.subject();
+  this.render();
+
+  Ember.run(function() {
+    component.set('checked', true);
+  });
+
+  assert.ok(this.$().hasClass('md-checked'));
+});
+
+test('it is possible to check and set selected property to value', function(assert) {
+  var component = this.subject();
+
+  assert.ok(!component.get('selected'));
+
+  var value = 'some value';
+
+  Ember.run(function() {
+    component.set('value', value);
+  });
+
+  this.$().trigger('click');
+
+  assert.equal(component.get('selected'), value);
+});
+
+test('it isn\'t possible to uncheck and set selected property to null', function(assert) {
+  var component = this.subject();
+
+  var value = 'some value';
+
+  Ember.run(function() {
+    component.set('value', value);
+    component.set('selected', value);
+  });
+
+  this.$().trigger('click');
+
+  //still has value
+  assert.equal(component.get('selected'), value);
+});
+
+test('it is possible to uncheck and set selected property to null if toggle is true', function(assert) {
+  var component = this.subject();
+
+  var value = 'some value';
+
+  Ember.run(function() {
+    component.set('value', value);
+    component.set('selected', value);
+    component.set('toggle', true);
+  });
+
+  this.$().trigger('click');
+
+  assert.equal(component.get('selected'), null);
 });
