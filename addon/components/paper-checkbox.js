e35142ed824a5cc08d591a9a2b87d60e3250f468
Miguel <jmandrade0@gmail.com>
2015-03-05 11:14:36 +0000

    complete checkbox tests and checkbox code styling

--- addon/components/paper-checkbox.js
+++ addon/components/paper-checkbox.js
@@ -3,32 +3,33 @@ import RippleMixin from '../mixins/ripple-mixin';
 
 var KEY_CODE_SPACE = 32;
 
-export default BaseFocusable.extend(RippleMixin,{
-  tagName:'md-checkbox',
-  classNames:['md-checkbox','md-default-theme'],
-  classNameBindings:['checked:md-checked'],
+export default BaseFocusable.extend(RippleMixin, {
+  tagName: 'md-checkbox',
+  classNames: ['md-checkbox', 'md-default-theme'],
+  classNameBindings: ['checked:md-checked'],
+
+  //Alow element to be focusable by supplying a tabindex 0
+  attributeBindings: ['tabindex'],
+  tabindex: function() {
+    return this.get('disabled') ? '-1' : '0';
+  }.property('disabled'),
+
+  checked: false,
+  toggle: true,
 
   /* RippleMixin overrides */
   center: true,
   dimBackground: false,
-  rippleContainerSelector:'.md-container',
+  rippleContainerSelector: '.md-container',
 
-  //Alow element to be focusable by supplying a tabindex 0
-  attributeBindings:['tabindex'],
-  tabindex:function(){
-    return this.get('disabled') ? '-1' : '0';
-  }.property('disabled'),
-  checked:false,
-
-  toggle:true,
-
-  click:function(){
-    if(!this.get('disabled')){
+  click: function() {
+    if (!this.get('disabled')) {
       this.toggleProperty('checked');
     }
   },
-  keyPress:function(ev){
-    if(ev.which === KEY_CODE_SPACE) {
+
+  keyPress: function(ev) {
+    if (ev.which === KEY_CODE_SPACE) {
       this.click();
     }
   }
--- tests/unit/components/paper-checkbox-test.js
+++ tests/unit/components/paper-checkbox-test.js
@@ -32,8 +32,70 @@ test('should set checked css class', function(assert) {
   assert.ok(this.$().hasClass('md-checked'));
 });
 
-test('blockless version should set label inside', function(assert){
+test('element should be focusable if not disabled', function(assert) {
   var component = this.subject();
+
+  assert.equal(this.$().attr('tabindex'), '0');
+});
+
+test('element should not be focusable if disabled', function(assert) {
+  var component = this.subject();
+  this.render();
+
+  Ember.run(function(){
+    component.set('disabled', true);
+  });
+
+  assert.equal(this.$().attr('tabindex'), '-1');
+});
+
+test('it is possible to check and set checked to true', function(assert) {
+  var component = this.subject();
+
+  this.$().trigger('click');
+
+  assert.ok(component.get('checked'));
+});
+
+test('it is possible to uncheck and set checked to false', function(assert) {
+  var component = this.subject();
+
+  Ember.run(function(){
+    component.set('checked', true);
+  });
+
+  this.$().trigger('click');
+
+  assert.ok(!component.get('checked'));
+});
+
+test('it is possible to check using spacebar', function(assert) {
+  var component = this.subject();
+
+  var e = $.Event('keypress');
+  e.which = 32; // # Some key code value
+  this.$().trigger(e);
+
+  assert.ok(component.get('checked'));
+});
+
+test('it is possible to uncheck using spacebar', function(assert) {
+  var component = this.subject();
+
+  Ember.run(function(){
+    component.set('checked', true);
+  });
+
+  var e = $.Event('keypress');
+  e.which = 32; // # Some key code value
+  this.$().trigger(e);
+
+  assert.ok(!component.get('checked'));
+});
+
+test('blockless version should set label inside', function(assert) {
+  var component = this.subject();
+  this.render();
 
   Ember.run(function(){
     component.set('label', 'Button');
