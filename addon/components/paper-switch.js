a7c62142698a70446b54ed63563bf23b62d732d9
Alex LaFroscia <alex@lafroscia.com>
2015-03-06 16:44:34 -0500

    Detect events with Hammer.js
    Adds event detection using Hammer.js.  Also moves some of the other logic to
    better match the Angular implementation.  Notably, the way that the amount to
    move the switch is calculated is much more efficient and the switch seems more
    responsive overall.  However, it's still a little off, and I'm not exactly sure
    how to fix it since I've never worked with Hammer.js before.  I'll keep playing
    with it, but maybe someone else can tell me what I'm doing wrong.

--- addon/components/paper-switch.js
+++ addon/components/paper-switch.js
@@ -15,36 +15,26 @@ export default BaseFocusable.extend(RippleMixin,{
   disabled: false,
 
   dragOffset: null,
+  switchWidth: null,
 
   _setupSwitchDragging: function() {
     // Don't set up anything if the switch is disabled
     if (this.get('disabled')) { return; }
 
+    this.set('switchWidth', this.$('.md-bar').width());
+
     // Enable dragging the switch
-    this.$('.md-thumb').on('mousedown', Ember.run.bind(this, this._dragStart));
+    var thumbElement = this.get('element').getElementsByClassName('md-thumb-container')[0];
+    var thumbElementHammer= new Hammer(thumbElement);
+    thumbElementHammer.get('pan').set({ pointers: 0, threshold: 1 });
+    thumbElementHammer.on('panleft', Ember.run.bind(this, this._drag));
+    thumbElementHammer.on('panright', Ember.run.bind(this, this._drag));
+    thumbElementHammer.on('panend', Ember.run.bind(this, this._dragEnd));
 
     // Allow the label of the button to remain clickable
-    this.$('.md-label')
-      .on('mousedown', Ember.run.bind(this, this._dragStart))
-      .on('mouseup', Ember.run.bind(this, this._dragEnd));
-    this.$('.md-bar')
-      .on('mousedown', Ember.run.bind(this, this._dragStart))
-      .on('mouseup', Ember.run.bind(this, this._dragEnd));
+    var switchHammer = new Hammer(this.get('element'));
+    switchHammer.on('tap', Ember.run.bind(this, this._dragEnd));
   }.on('didInsertElement').observes('disabled'),
-
-
-  _teardownSwitchDragging: function() {
-    if (!this.get('disabled')) { return; }
-    this.$('.md-thumb-container').off('mousedown');
-    this.$('.md-label').off('mousedown').off('mouseup');
-  }.observes('disabled'),
-
-
-  _dragStart: function() {
-    // Set up the listeners for mouse movement and the end of the click
-    Ember.$('.ember-application').on('mousemove', Ember.run.bind(this, this._drag));
-    Ember.$('.ember-application').on('mouseup', Ember.run.bind(this, this._dragEnd));
-  },
 
 
   /**
@@ -55,27 +45,27 @@ export default BaseFocusable.extend(RippleMixin,{
    * switch, it can be converted into a percentage to move the switch itself by.
    */
   _drag: function(event) {
-    var switchLeftOffset = this.$().offset().left;
-    var eventLeft = event.clientX - switchLeftOffset;
-    var switchWidth = 34;
-    var offsetLeft = eventLeft / switchWidth;
-    offsetLeft = Math.max(0, Math.min(1, offsetLeft)) * 100;
-    offsetLeft = Math.floor(offsetLeft);
-    this.set('dragOffset', offsetLeft);
-    this.$('.md-thumb-container').css('transform', 'translate3d(' + offsetLeft + '%, 0, 0');
-    this.$('.md-thumb-container').css('-webkit-transform', 'translate3d(' + offsetLeft + '%, 0, 0');
+    if (this.get('disabled')) { return; }
+
+    var percent = event.deltaX / this.get('switchWidth');
+    var translate = Math.max(0, Math.min(1, percent));
+    this.set('dragOffset', translate);
+    var transformProp = 'translate3d(' + (100 * translate) + '%, 0, 0)';
+    this.$('.md-thumb-container').css('transform', transformProp);
+    this.$('.md-thumb-container').css('-webkit-transform', transformProp);
   },
 
 
   _dragEnd: function() {
-    // Toggle the value
+    if (this.get('disabled')) { return; }
+
     if (this.get('dragOffset') === null) {
       // Switch was just clicked
       this.toggleProperty('checked');
-    } else if (this.get('checked') && this.get('dragOffset') < 50) {
+    } else if (this.get('checked') && this.get('dragOffset') < 0.5) {
       // Switch was checked and dragged past the halfway point
       this.set('checked', false);
-    } else if (!this.get('checked') && this.get('dragOffset') > 50) {
+    } else if (!this.get('checked') && this.get('dragOffset') > 0.5) {
       // Switch was not checked and dragged past the halfway point
       this.set('checked', true);
     }
@@ -83,8 +73,6 @@ export default BaseFocusable.extend(RippleMixin,{
     // Cleanup
     this.set('dragOffset', null);
     this.$('.md-thumb-container').removeAttr('style');
-    Ember.$('.ember-application').off('mousemove');
-    Ember.$('.ember-application').off('mouseup');
   }
 
 });
