50a507ca8e4b656084fd29344609f0a2bae061eb
Alex LaFroscia <alex@lafroscia.com>
2015-03-06 17:21:00 -0500

    Got switch movement working properly
    - Adds the `md-dragging` class to the switch when drag is taking place
    - Correctly tracks the mouse movement during the drag
    - Cleans up the code a bit

--- addon/components/paper-switch.js
+++ addon/components/paper-switch.js
@@ -5,7 +5,7 @@ import RippleMixin from '../mixins/ripple-mixin';
 export default BaseFocusable.extend(RippleMixin,{
   tagName:'md-switch',
   classNames:['paper-switch','md-default-theme'],
-  classNameBindings:['checked:md-checked'],
+  classNameBindings:['checked:md-checked', 'dragging:md-dragging'],
   toggle:true,
 
   center: true,
@@ -14,7 +14,8 @@ export default BaseFocusable.extend(RippleMixin,{
   checked: false,
   disabled: false,
 
-  dragOffset: null,
+  dragging: false,
+  dragAmount: null,
   switchWidth: null,
 
   _setupSwitchDragging: function() {
@@ -26,30 +27,32 @@ export default BaseFocusable.extend(RippleMixin,{
     // Enable dragging the switch
     var thumbElement = this.get('element').getElementsByClassName('md-thumb-container')[0];
     var thumbElementHammer= new Hammer(thumbElement);
-    thumbElementHammer.get('pan').set({ pointers: 0, threshold: 1 });
-    thumbElementHammer.on('panleft', Ember.run.bind(this, this._drag));
-    thumbElementHammer.on('panright', Ember.run.bind(this, this._drag));
+    thumbElementHammer.get('pan').set({ threshold: 1 });
+    thumbElementHammer.on('panstart', Ember.run.bind(this, this._dragStart));
+    thumbElementHammer.on('panmove', Ember.run.bind(this, this._drag));
     thumbElementHammer.on('panend', Ember.run.bind(this, this._dragEnd));
 
-    // Allow the label of the button to remain clickable
+    // Allow the switch to be clicked to toggle the value
     var switchHammer = new Hammer(this.get('element'));
     switchHammer.on('tap', Ember.run.bind(this, this._dragEnd));
   }.on('didInsertElement').observes('disabled'),
 
 
-  /**
-   * Handles moving the switch incrementally as it is dragged by the mouse.
-   * Gets the left offset of the switch and the place that the cursor is during
-   * the drag, and subtracts them to get the amount of pixels from the left of
-   * the switch the mouse is at.  By dividing this amount by by width of the
-   * switch, it can be converted into a percentage to move the switch itself by.
-   */
+  _dragStart: function() {
+    this.set('dragging', true);
+  },
+
+
   _drag: function(event) {
     if (this.get('disabled')) { return; }
 
+    // Get the amount amount the switch has been dragged
     var percent = event.deltaX / this.get('switchWidth');
+    percent = this.get('checked') ? 1 + percent : percent;
+    this.set('dragAmount', percent);
+
+    // Make sure that the switch isn't moving past the edges
     var translate = Math.max(0, Math.min(1, percent));
-    this.set('dragOffset', translate);
     var transformProp = 'translate3d(' + (100 * translate) + '%, 0, 0)';
     this.$('.md-thumb-container').css('transform', transformProp);
     this.$('.md-thumb-container').css('-webkit-transform', transformProp);
@@ -59,20 +62,16 @@ export default BaseFocusable.extend(RippleMixin,{
   _dragEnd: function() {
     if (this.get('disabled')) { return; }
 
-    if (this.get('dragOffset') === null) {
-      // Switch was just clicked
+    if ( (!this.get('dragging')) ||
+         (this.get('checked') && this.get('dragAmount') < 0.5) ||
+         (!this.get('checked') && this.get('dragAmount') > 0.5)) {
       this.toggleProperty('checked');
-    } else if (this.get('checked') && this.get('dragOffset') < 0.5) {
-      // Switch was checked and dragged past the halfway point
-      this.set('checked', false);
-    } else if (!this.get('checked') && this.get('dragOffset') > 0.5) {
-      // Switch was not checked and dragged past the halfway point
-      this.set('checked', true);
     }
 
     // Cleanup
-    this.set('dragOffset', null);
     this.$('.md-thumb-container').removeAttr('style');
+    this.set('dragging', false);
+    this.set('dragAmount', null);
   }
 
 });
