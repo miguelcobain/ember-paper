9904e9f6265b5030f02f0734c98a794c7948f479
Alex LaFroscia <alex@lafroscia.com>
2015-03-08 15:06:11 -0400

    Get tap event with Hammer
    For some reason the default `click` event wasn't working on iOS.  I swapped it
    out with a Hammer event and it seems to work much better now.

--- addon/components/paper-backdrop.js
+++ addon/components/paper-backdrop.js
@@ -4,8 +4,14 @@ export default Ember.Component.extend({
   tagName: 'md-backdrop',
   classNames: ['paper-backdrop', 'md-opaque', 'md-default-theme'],
 
-  click: function(evt) {
-    Ember.$(evt.target).trigger('collapseSidenav');
+  subscribeToTouchEvents: function() {
+    var hammer = new Hammer(this.get('element'));
+    hammer.on('tap', Ember.run.bind(this, this._collapseSidenav));
+  }.on('didInsertElement'),
+
+  _collapseSidenav: function(event) {
+    Ember.$(event.target).trigger('collapseSidenav');
     return false;
   }
+
 });
