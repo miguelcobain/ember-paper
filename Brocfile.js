82117ce362081b7959f4d41a559fe3bc6e42ed4f
Brandon J McKay <bmckay@rew.ca>
2015-04-27 10:49:52 -0700

    Add ember-prism for better code highlighting
    The code examples on the demo site were hard to read, so I added the PrismJS library for better code highlighting. I created components to use for it, moved it out to an ember-prism addon, and added that as a dependency here.

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,11 @@
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-var app = new EmberAddon();
+var app = new EmberAddon({
+  'ember-prism': {
+    'components': ['javascript', 'handlebars', 'scss', 'bash']
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
