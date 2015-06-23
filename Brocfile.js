72919016781484bcbcb8a233af0ea306f900dee4
Miguel Andrade <jmandrade0@gmail.com>
2015-06-23 16:36:02 +0100

    initial gh-pages commit

---
+++ Brocfile.js
@@ -0,0 +1,20 @@
+/* jshint node: true */
+/* global require, module */
+
+var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+/*
+  This Brocfile specifes the options for the dummy test app of this
+  addon, located in `/tests/dummy`
+
+  This Brocfile does *not* influence how the addon or the app using it
+  behave. You most likely want to be modifying `./index.js` or app's Brocfile
+*/
+
+var app = new EmberAddon({
+  'ember-prism': {
+    'components': ['javascript', 'handlebars', 'scss', 'bash']
+  }
+});
+
+module.exports = app.toTree();
