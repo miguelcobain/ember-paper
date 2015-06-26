27625f16b97723ac79d8ad0c92ad535436c16125
Chris Manson <chris@manson.ie>
2015-06-26 11:51:27 +0100

    adding the ability to include custom templates

--- tests/dummy/app/controllers/chips.js
+++ tests/dummy/app/controllers/chips.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   fruitNames: Ember.A(['Apple', 'Banana', 'Orange']),
 
+  customFruitNames: Ember.A(['Apple', 'Banana', 'Orange']),
+
   actions: {
     removeItem(item){
       this.get('fruitNames').removeObject(item);
@@ -10,6 +12,14 @@ export default Ember.Controller.extend({
 
     addItem(item){
       this.get('fruitNames').pushObject(item);
+    },
+
+    removeCustomItem(item){
+      this.get('customFruitNames').removeObject(item);
+    },
+
+    addCustomItem(item){
+      this.get('customFruitNames').pushObject(item);
     }
   }
 });
