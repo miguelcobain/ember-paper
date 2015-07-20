05126b3174df9dd69792e7542f19f2188ab2bc74
Miguel Andrade <jmandrade0@gmail.com>
2015-07-20 01:26:51 +0100

    update to ember-cli 1.13.1

--- tests/unit/components/paper-switch-test.js
+++ tests/unit/components/paper-switch-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('paper-switch', 'PaperToggleComponent', {
+moduleForComponent('paper-switch', 'PaperSwitchComponent', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
   unit: true
