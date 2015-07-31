6c02808fefa7cbd01cfba9a4b49d158c1eeaf774
Petter Kjelkenes <kjelkenes@gmail.com>
2015-07-31 22:45:00 +0200

    Add global toast example (this will probably be the most used) by triggering action to send messages from any route..

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -5,8 +5,18 @@ export default Ember.Route.extend({
     willTransition() {
       this.controller.set('drawerOpen', false);
     },
+    
     alertValue(value) {
       alert(`You clicked Radio button: ${value}`);
+    },
+
+    openGlobalToast(msg) {
+      this.controller.set('globalToastMessage', msg);
+      this.controller.set('isOpenGlobalToast', true);
+    },
+
+    closeGlobalToast() {
+      this.controller.set('isOpenGlobalToast', false);
     }
   }
 });
