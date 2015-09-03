2dc2b63223563b5e99ca2a62846ef9edc0d85f60
Christian Anese <anese.c@trick17.it>
2015-09-03 14:55:12 +0200

    Added basic dialog setup with small demo page

--- addon/components/paper-dialog.js
+++ addon/components/paper-dialog.js
@@ -1,6 +1,93 @@
 import Ember from 'ember';
+import TransitionMixin from 'ember-css-transitions/mixins/transition-mixin';
 
+export default Ember.Component.extend(TransitionMixin, {
+  tagName: 'md-dialog',
+  classNames: ['md-default-theme'],
+  classNameBindings: ['overflow:md-content-overflow'],
 
-export default Ember.Component.extend({
+  parent: null,
+  title: '',
+  content: '',
+  ariaLabel: '',
+  role: 'dialog',
+  hasBackdrop: true,
+  clickOutsideToClose: true,
+  closeOnConfirm: false,
 
+  transitionClass: 'md-transition',
+
+  hasActions: Ember.computed('property', function() {
+    return Ember.isPresent(this.get('onConfirm')) || Ember.isPresent(this.get('onCancel'));
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.setupElement();
+
+    Ember.run.schedule('afterRender', () => {
+      this.checkContentOverflow();
+    });
+
+    if (this.get('clickOutsideToClose')) {
+      this.$().parent().on('click touchend', Ember.run.bind(this, 'close') );
+    }
+  },
+
+  setupElement(){
+    Ember.$('body').addClass('md-dialog-is-showing');
+    this.$().addClass('md-transition-in');
+
+    this.$().attr('role',this.get('role'));
+    this.$().attr('tabIndex',-1);
+
+    this.get('parentElement').append(this.$());
+
+    if (this.get('hasBackdrop')) {
+      this.get('parentElement').prepend('<md-backdrop class="md-dialog-backdrop md-opaque ng-scope md-default-theme" aria-hidden="true"></md-backdrop>');
+    }
+
+    this.$().wrap('<div class="md-dialog-container ng-scope"></div>');
+  },
+
+  checkContentOverflow(){
+    var content = this.$('md-dialog-content')[0];
+
+    if (content.scrollHeight > content.clientHeight){
+      this.set('overflow',true);
+    }
+  },
+
+  willDestroyElement() {
+    Ember.$('body').removeClass('md-dialog-is-showing');
+
+    this.$().parent().remove();
+    this.get('parentElement').children('md-backdrop').remove();
+  },
+  close(event){
+    if(this.$().find(event.target).length === 0){
+      this.$().parent().off('click touchend');
+      this.send('onCancel');
+    }
+  },
+  parentElement: Ember.computed('parent', function() {
+      return this.get('parent') ? Ember.$(this.get('parent')) : this.$().parent();
+  }),
+
+  actions: {
+    onConfirm() {
+      this.sendAction('onConfirm');
+
+      if (this.get('closeOnConfirm')) {
+        this.send('onCancel');
+      }
+    },
+    onCancel(){
+      this.$().removeClass('md-transition-in');
+      this.$().addClass('md-transition-out');
+
+      this.sendAction('onCancel');
+    },
+  }
 });
--- tests/dummy/app/routes/dialog.js
+++ tests/dummy/app/routes/dialog.js
@@ -1,4 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    showAlert() {
+      this.controller.set('isAlert',true);
+    },
+    showConfirm() {
+      this.controller.set('isConfirm',true);
+    },
+    showCustom() {
+      this.controller.set('isCustom',true);
+    },
+    closeAlert(){
+      this.controller.set('isAlert',false);
+    },
+    closeConfirm() {
+      this.controller.set('isConfirm',false);
+    },
+    closeCustom() {
+      this.controller.set('isCustom',false);
+    },
+    handleDialog(){
+      window.alert('Handling action');
+    }
+  }
 });
