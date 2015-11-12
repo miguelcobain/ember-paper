8a161aaad7e954139da4af4a2859180e3383f2ae
Christian Anese <christian.anese@gmail.com>
2015-11-12 14:34:37 +0100

    Fixed failing test

--- tests/integration/components/paper-fab-action-item-test.js
+++ tests/integration/components/paper-fab-action-item-test.js
@@ -6,7 +6,7 @@ moduleForComponent('paper-fab-action-item', 'Integration | Component | paper fab
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(1);
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
@@ -14,13 +14,4 @@ test('it renders', function(assert) {
   this.render(hbs`{{paper-fab-action-item}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#paper-fab-action-item}}
-      template block text
-    {{/paper-fab-action-item}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
