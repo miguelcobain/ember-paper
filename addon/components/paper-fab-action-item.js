29b4e3b1d7ad090a17a1236b3d28f4adfe53d1ec
Christian Anese <christian.anese@gmail.com>
2015-11-17 13:41:13 +0100

    Fixed issue with actions

--- addon/components/paper-fab-action-item.js
+++ addon/components/paper-fab-action-item.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: 'md-fab-action-item',
 
-  icon: 'menu'
+  icon: 'menu',
+
+  //bubble actions by default
+  bubbles: true,
+  click() {
+    var target = this.get('target');
+
+    if (target) {
+      this.get('target').send(this.get('action'), this.get('param'));
+    } else {
+      this.sendAction('action', this.get('param'));
+    }
+
+    return this.get('bubbles');
+  }
 });
