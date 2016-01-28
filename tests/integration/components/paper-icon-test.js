fbc6de1f2faa821aa8c99f244de2df1fa0685496
Elad Shahar <SaladFork@Gmail.com>
2016-01-27 22:47:47 -0500

    Add tests for `paper-icon`

---
+++ tests/integration/components/paper-icon-test.js
@@ -0,0 +1,86 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('paper-icon', 'Integration | Component | paper icon', {
+    integration: true
+});
+
+test('it renders with tag name', function(assert) {
+    assert.expect(1);
+
+    this.render(hbs`{{paper-icon}}`);
+
+    assert.ok(this.$('md-icon').length);
+});
+
+test('it renders with classes', function(assert) {
+    assert.expect(6);
+
+    this.set('icon', 'foo');
+    this.render(hbs`{{paper-icon icon=icon}}`);
+
+    const $component = this.$('md-icon');
+
+    assert.ok($component.hasClass('paper-icon'));
+    assert.ok($component.hasClass('material-icons'));
+    assert.ok($component.hasClass('foo'));
+    assert.ok($component.hasClass('md-default-theme'));
+
+    this.set('icon', 'bar');
+    assert.ok($component.hasClass('bar'));
+    assert.notOk($component.hasClass('foo'));
+});
+
+test('it renders with spin class', function(assert) {
+    assert.expect(2);
+
+    this.set('spin', true);
+    this.render(hbs`{{paper-icon spin=spin}}`);
+
+    var $component = this.$('md-icon');
+
+    assert.ok($component.hasClass('md-spin'));
+
+    this.set('spin', false);
+    assert.notOk($component.hasClass('md-spin'));
+});
+
+test('it renders with reverse spin class', function(assert) {
+    assert.expect(2);
+
+    this.set('reverseSpin', true);
+    this.render(hbs`{{paper-icon reverseSpin=reverseSpin}}`);
+
+    var $component = this.$('md-icon');
+
+    assert.ok($component.hasClass('md-spin-reverse'));
+
+    this.set('reverseSpin', false);
+    assert.notOk($component.hasClass('md-spin-reverse'));
+});
+
+test('it renders with size class', function(assert) {
+    assert.expect(6);
+
+    this.set('size', 'lg');
+    this.render(hbs`{{paper-icon size=size}}`);
+
+    var $component = this.$('md-icon');
+
+    assert.ok($component.hasClass('md-lg'));
+
+    this.set('size', 'sm');
+    assert.ok($component.hasClass('md-sm'));
+
+    this.set('size', 2);
+    assert.ok($component.hasClass('md-2x'));
+
+    this.set('size', 3);
+    assert.ok($component.hasClass('md-3x'));
+
+    this.set('size', 4);
+    assert.ok($component.hasClass('md-4x'));
+
+    this.set('size', 5);
+    assert.ok($component.hasClass('md-5x'));
+});
