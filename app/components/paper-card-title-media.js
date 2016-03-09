a6bc80c44d6aff31a7ee802b825dc0c68ec4d9dd
Bailey Miller <baileymiller182@gmail.com>
2016-03-08 22:32:32 -0800

    Add paper-card-title components

---
+++ app/components/paper-card-title-media.js
@@ -0,0 +1,4 @@
+import PaperCardTitleMedia from 'ember-paper/components/paper-card-title-media';
+
+export default PaperCardTitleMedia;
+
---
+++ app/components/paper-card-title.js
@@ -0,0 +1,3 @@
+import PaperCardTitle from 'ember-paper/components/paper-card-title';
+
+export default PaperCardTitle;
