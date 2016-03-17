06b9fe173fda25667d1806415307dfb5880872d4
Steven <steven@hazeltree.org>
2016-03-17 10:55:22 -0700

    jscs changes

--- addon/components/paper-tab-content.js
+++ addon/components/paper-tab-content.js
@@ -1,22 +1,27 @@
 import Ember from 'ember';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
-  tabsBinding: 'parent.tabs',
-  activeTabBinding: 'parent.activeTab',
-  wormholeBinding: 'parent.wormhole',
-  positionBinding: 'parent.position',
-  activeTabIndexBinding: 'parent.activeTabIndex',
-  tabIndexBinding: 'parent.tabIndex',
-  isActiveBinding: 'parent.isActive',
+  tabs: computed.readOnly('parent.tabs'),
+  activeTab: computed.readOnly('parent.activeTab'),
+  wormhole: computed.readOnly('parent.wormhole'),
+  position: computed.readOnly('parent.position'),
+  activeTabIndex: computed.readOnly('parent.activeTabIndex'),
+  tabIndex: computed.readOnly('parent.tabIndex'),
+  isActive: computed.readOnly('parent.isActive'),
 
-  isLeft: Ember.computed('isActive', 'tabIndex', 'activeTabIndex', function(){
-    if (this.get('isActive')) { return false; }
+  isLeft: Ember.computed('isActive', 'tabIndex', 'activeTabIndex', function() {
+    if (this.get('isActive')) {
+      return false;
+    }
     if (this.get('tabIndex') < this.get('activeTabIndex')) {
       return true;
     }
   }),
-  isRight: Ember.computed('isActive', 'tabIndex', 'activeTabIndex', function(){
-    if (this.get('isActive')) { return false; }
+  isRight: Ember.computed('isActive', 'tabIndex', 'activeTabIndex', function() {
+    if (this.get('isActive')) {
+      return false;
+    }
     if (this.get('tabIndex') > this.get('activeTabIndex')) {
       return true;
     }
