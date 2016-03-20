437bf47995dc7d9bcdb8202d6f4ab60d3d731ab7
Dan Chadwick <dan899@gmail.com>
2016-03-20 17:24:16 -0400

    Fix fullscreen and update dummy app to demonstrate fullscreen and primary-colored buttons.

--- addon/components/paper-dialog-inner.js
+++ addon/components/paper-dialog-inner.js
@@ -7,7 +7,7 @@ const { Component, computed } = Ember;
 export default Component.extend(/*Translate3dMixin, TODO */ {
   tagName: 'md-dialog',
   classNames: ['md-default-theme'     , 'md-transition-in'     /* TODO */ ],
-  classNameBindings: ['contentOverflow:md-content-overflow', 'fullscreen:md-dialog-fullscreen'],
+  classNameBindings: ['contentOverflow:md-content-overflow', 'dialogComponent.fullscreen:md-dialog-fullscreen'],
 
   dialogComponent: computed(function() {
     return this.nearestOfType(PaperDialog);
