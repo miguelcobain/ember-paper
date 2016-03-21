b59d2ab1ef8848843df0bfdc2773db4bc25285dd
Dan Chadwick <dan899@gmail.com>
2016-03-21 16:40:58 -0400

    Bind fullscreen and clickOutsideToClose in paper-dialog-inner, rather than use nearestOfType to find the dialog component.

--- addon/components/paper-dialog-inner.js
+++ addon/components/paper-dialog-inner.js
@@ -7,22 +7,18 @@ const { Component, computed } = Ember;
 export default Component.extend(/*Translate3dMixin, TODO */ {
   tagName: 'md-dialog',
   classNames: ['md-default-theme'     , 'md-transition-in'     /* TODO */ ],
-  classNameBindings: ['contentOverflow:md-content-overflow', 'dialogComponent.fullscreen:md-dialog-fullscreen'],
+  classNameBindings: ['contentOverflow:md-content-overflow', 'fullscreen:md-dialog-fullscreen'],
 
-  dialogComponent: computed(function() {
-    return this.nearestOfType(PaperDialog);
-  }),
+  translateFromOrigin: computed.reads('openFrom'),
 
-  translateFromOrigin: computed.reads('dialogComponent.openFrom'),
-
-  translateToParent: computed.reads('dialogComponent.closeTo'),
+  translateToParent: computed.reads('closeTo'),
 
   onTranslateDestroy(origin) {
     origin.focus();
   },
 
   click(ev) {
-    if (this.get('dialogComponent.clickOutsideToClose')) {
+    if (this.get('clickOutsideToClose')) {
       ev.stopPropagation();
       return false;
     }
