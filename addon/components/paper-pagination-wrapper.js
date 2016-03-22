c49845d63de0901cb830a48d6a41d87c2c4076dd
Steven <steven@hazeltree.org>
2016-03-21 21:45:24 -0700

    initial support for `md-ink-bar` animation

--- addon/components/paper-pagination-wrapper.js
+++ addon/components/paper-pagination-wrapper.js
@@ -4,9 +4,24 @@ const { computed } = Ember;
 export default Ember.Component.extend({
   tagName: 'md-pagination-wrapper',
 
-  // inherited
+  /* Inherited */
   tabs: computed.reads('parent.tabs'),
   centerTabs: computed.reads('parent.centerTabs'),
+  selected: computed.reads('parent.selected'),
+  lastSelectedIndex: computed.reads('parent.lastSelectedIndex'),
+  selectedTab: computed.reads('parent.selectedTab'),
+  noInkBar: computed.reads('parent.noInkBar'),
+
+  showInkBar: computed('noInkBar', 'tabs.[]', function() {
+    if (!this.get('noInkBar') && (this.get('tabs.length') > 0)) {
+      return true;
+    }
+    return false;
+  }),
+
+  inkBarDirection: computed('lastSelectedIndex', 'selected', function() {
+    return (this.get('lastSelectedIndex') > this.get('selected')) ? 'left' : 'right';
+  }),
 
   classNameBindings: ['centerTabs:md-center-tabs'],
   attributeBindings: ['styleAttr:style'],
@@ -18,13 +33,26 @@ export default Ember.Component.extend({
     if (this.get('shouldStrechTabs')) {
       return '';
     } else {
-      return (this.get('width')) ? `width: ${this.get('width')} px;` : 'width: 0px';
+      return (this.get('width')) ? `width: ${this.get('width')}px;` : 'width: 0px';
     }
+  }),
+
+  inkBarLeftPosition: computed('selectedTab.left', function() {
+    let leftOffset = this.$().offset().left;
+    return this.get('selectedTab.left') - leftOffset;
+  }),
+  inkBarRightPosition: computed('inkBarLeftPosition', 'selectedTab.width', 'width', function() {
+    let leftOffset = this.$().offset().left;
+    let position = this.get('width');// - this.get('selectedTab.left');
+    position -= this.get('inkBarLeftPosition');
+    position -= this.get('selectedTab.width');
+    return position;
+    // return (this.get('parent.selectedTab.left') + this.get('parent.selectedTab.width')) - this.get('wrapperWidth');
   }),
 
   shouldStrechTabs: false, // todo: implement computed property
 
-  width: computed('tabs.[]', 'shouldStrechTabs', function() {
+  width: computed('tabs.[].id', 'shouldStretchTabs', function() {
     let context = this;
     let width = 1;
     this.get('tabs').forEach(function(tab) {
