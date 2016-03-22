849cc1a12fd2eea68b10f40f09a86b249340c354
Steven <steven@hazeltree.org>
2016-03-22 12:19:07 -0700

    pagination improvements

--- addon/components/paper-pagination-wrapper.js
+++ addon/components/paper-pagination-wrapper.js
@@ -4,13 +4,15 @@ const { computed } = Ember;
 export default Ember.Component.extend({
   tagName: 'md-pagination-wrapper',
 
-  /* Inherited */
+  /* Inherited from {{paper-tabs-wrapper}} */
   tabs: computed.reads('parent.tabs'),
   centerTabs: computed.reads('parent.centerTabs'),
   selected: computed.reads('parent.selected'),
   lastSelectedIndex: computed.reads('parent.lastSelectedIndex'),
   selectedTab: computed.reads('parent.selectedTab'),
   noInkBar: computed.reads('parent.noInkBar'),
+  canvasWidth: computed.reads('parent.canvasWidth'),
+  pagingWidth: computed.reads('parent.pagingWidth'),
 
   showInkBar: computed('noInkBar', 'tabs.[]', function() {
     if (!this.get('noInkBar') && (this.get('tabs.length') > 0)) {
@@ -29,11 +31,12 @@ export default Ember.Component.extend({
     return this.get('widthStyle');
   }),
 
-  widthStyle: computed('width', function() {
+  widthStyle: computed('pagingWidth', function() {
     if (this.get('shouldStrechTabs')) {
       return '';
     } else {
-      return (this.get('width')) ? `width: ${this.get('width')}px;` : 'width: 0px';
+      let width = this.get('pagingWidth')
+      return (width) ? `width: ${width}px;` : 'width: 0px';
     }
   }),
 
@@ -41,9 +44,9 @@ export default Ember.Component.extend({
     let leftOffset = this.$().offset().left;
     return this.get('selectedTab.left') - leftOffset;
   }),
-  inkBarRightPosition: computed('inkBarLeftPosition', 'selectedTab.width', 'width', function() {
+  inkBarRightPosition: computed('inkBarLeftPosition', 'selectedTab.width', 'pagingWidth', function() {
     let leftOffset = this.$().offset().left;
-    let position = this.get('width');// - this.get('selectedTab.left');
+    let position = this.get('pagingWidth');// - this.get('selectedTab.left');
     position -= this.get('inkBarLeftPosition');
     position -= this.get('selectedTab.width');
     return position;
@@ -51,16 +54,4 @@ export default Ember.Component.extend({
   }),
 
   shouldStrechTabs: false, // todo: implement computed property
-
-  width: computed('tabs.[].id', 'shouldStretchTabs', function() {
-    let context = this;
-    let width = 0;
-    this.get('tabs').forEach(function(tab) {
-      if (tab.id) {
-        let element = document.getElementById(tab.id);
-        width += Math.max(element.offsetWidth, element.getBoundingClientRect().width);
-      }
-    });
-    return Math.ceil(width);
-  })
 });
