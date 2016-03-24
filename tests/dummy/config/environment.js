0a91fc11295e6faa74abb84bb9d5fc6b7a71909e
Dan Chadwick <dan899@gmail.com>
2016-03-24 15:29:56 -0400

    environment.js clean-up.

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,9 +1,13 @@
-/* jshint node: true */
+// app/config/environment.js
 
 module.exports = function(environment) {
-  var ENV = {
+  // jshint node: true
+  // jscs:disable requireEnhancedObjectLiterals, validateQuoteMarks, disallowEmptyBlocks
+  'use strict';
+
+  let ENV = {
     modulePrefix: 'dummy',
-    environment: environment,
+    environment,
     baseURL: '/',
     locationType: 'hash',
     EmberENV: {
@@ -16,42 +20,45 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-      rootElement: "body"
+      rootElement: 'body'
     },
     contentSecurityPolicy: {
       'default-src': "'none'",
-      'script-src': "'self' 'unsafe-inline' https://buttons.github.io https://api.github.com",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' https://buttons.github.io https://api.github.com",
       'font-src': "'self' https://fonts.gstatic.com",
       'style-src': "'self' 'unsafe-inline' https://buttons.github.io https://fonts.googleapis.com",
       'connect-src': "'self'",
       'img-src': "'self' data:",
       'media-src': "'self'",
-      'frame-src': "https://buttons.github.io"
+      'child-src': "'self' https://buttons.github.io"
     }
   };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+  switch (environment) {
+    case 'development': {
+      // ENV.APP.LOG_RESOLVER = true;
+      // ENV.APP.LOG_ACTIVE_GENERATION = true;
+      // ENV.APP.LOG_TRANSITIONS = true;
+      // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+      // ENV.APP.LOG_VIEW_LOOKUPS = true;
+      break;
+    }
+    case 'test': {
+      // Testem prefers this...
+      ENV.baseURL = '/';
+      ENV.locationType = 'none';
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+      // keep test console output quieter
+      ENV.APP.LOG_ACTIVE_GENERATION = false;
+      ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
-  }
-
-  if (environment === 'production') {
-    ENV.baseURL = '/ember-paper';
+      ENV.APP.rootElement = '#ember-testing';
+      break;
+    }
+    case 'production': {
+      ENV.baseURL = '/ember-paper';
+      break;
+    }
   }
 
   return ENV;
