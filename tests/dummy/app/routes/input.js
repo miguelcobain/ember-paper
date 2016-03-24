e53fee416caa5c55df976443b24cd38475ad0c33
Dan Chadwick <dan899@gmail.com>
2016-03-24 09:30:36 -0400

    paper-input: Fix indenting and style in documentation page.

--- tests/dummy/app/routes/input.js
+++ tests/dummy/app/routes/input.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const { Route } = Ember;
+
+export default Route.extend({
   model() {
     // Create a new record for demonstration purposes. Normally, data
     // would be retrieved from the store via find or findAll.
