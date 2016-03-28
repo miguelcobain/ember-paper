bdcc36920684203bece6bd8c480b16cafb85dc1b
Steven <steven@hazeltree.org>
2016-03-28 14:18:31 -0700

    added integration testing for tabs

--- addon/components/paper-tab-label.js
+++ addon/components/paper-tab-label.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: ''
+  tagName: '',
+  didInsertElement() {
+    if (this.get('parent')) {
+      this.get('parent').send('identifyTabLabel', this.elementId);
+    }
+  }
 });
