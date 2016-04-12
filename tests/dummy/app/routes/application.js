83c1d8f387c36cc6b59c799ebd78bd70d22e1ffc
Miguel Andrade <jmandrade0@gmail.com>
2016-04-12 22:45:49 +0100

    more dummy app fixes

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,15 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    raisedButton() {
-      alert('You pressed a raised button.');
-    },
-    flatButton() {
-      alert('You pressed a flat button.');
-    },
-    targetButton() {
-      alert('You pressed a target button. -from route');
-    },
     willTransition() {
       this.controller.set('drawerOpen', false);
     },
