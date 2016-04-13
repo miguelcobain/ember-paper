31751b6203d89f47a5f17ee5c03975b0d37d9e10
Dan Chadwick <dan899@gmail.com>
2016-04-13 07:18:00 -0400

    paper-select: Add assert and test `onChange` action.

--- addon/components/paper-select.js
+++ addon/components/paper-select.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
 import PaperInput from './paper-input';
 
+let { assert } = Ember;
+
 export default PaperInput.extend({
   label: Ember.computed.alias('placeholder'),
   value: Ember.computed.alias('model'),
   itemLabelCallback: Ember.computed.alias('item-label-callback'),
-  onOpen: Ember.computed.alias('on-open')
+  onOpen: Ember.computed.alias('on-open'),
+
+  // Lifecycle hooks
+  didReceiveAttrs() {
+    this._super(...arguments);
+    assert('{{paper-select}} requires an `onChange` action', !!this.get('onChange'));
+  }
+
 });
---
+++ tests/integration/components/paper-select-test.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('paper-select', 'Integration | Component | paper select', {
+  integration: true
+});
+
+test('the `onChange` action is mandatory', function(assert) {
+  assert.expect(1);
+
+  assert.throws(() => {
+    this.render(hbs`{{paper-select}}`);
+  }, /requires an `onChange` action/);
+});
