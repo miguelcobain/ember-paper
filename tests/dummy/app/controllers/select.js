d7d7b8db7d67cc6f9de543f31a08986b5f609a23
Dan Chadwick <dan899@gmail.com>
2016-04-13 07:35:02 -0400

    paper-select: Fix state example to not show blank states.

--- tests/dummy/app/controllers/select.js
+++ tests/dummy/app/controllers/select.js
@@ -4,9 +4,8 @@ export default Ember.Controller.extend({
 
   userState: '',
   states: Ember.computed(function() {
-    return (`AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS
-      MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI
-      WY`).split(' ').map((state) => ({ abbrev: state.trim() }));
+    return 'AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY'
+            .split(' ').map((state) => ({ abbrev: state }));
   }),
 
   sizes: Ember.A([
