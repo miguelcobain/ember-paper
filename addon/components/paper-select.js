9b2c505ac3de804c279556fe2c1c45696c77a8c6
Dan Chadwick <dan899@gmail.com>
2016-04-13 12:01:52 -0400

    paper-select, paper-input: Combine assertion for `onChange`. Coding style update.
    When paper-select calls _super() in didReciveAttrs, it will trigger paper-input's assertion, not its own.
    Rather than assert before the call to _super, simply combine the messages.

--- addon/components/paper-select.js
+++ addon/components/paper-select.js
@@ -1,18 +1,11 @@
 import Ember from 'ember';
 import PaperInput from './paper-input';
 
-let { assert } = Ember;
+let { computed } = Ember;
 
 export default PaperInput.extend({
-  label: Ember.computed.alias('placeholder'),
-  value: Ember.computed.alias('model'),
-  itemLabelCallback: Ember.computed.alias('item-label-callback'),
-  onOpen: Ember.computed.alias('on-open'),
-
-  // Lifecycle hooks
-  didReceiveAttrs() {
-    this._super(...arguments);
-    assert('{{paper-select}} requires an `onChange` action', !!this.get('onChange'));
-  }
-
+  label: computed.alias('placeholder'),
+  value: computed.alias('model'),
+  itemLabelCallback: computed.alias('item-label-callback'),
+  onOpen: computed.alias('on-open')
 });
