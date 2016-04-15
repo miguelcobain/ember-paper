fa8f1cb8e24f109f1be5a2eba4124f6e612e375f
Dan Chadwick <dan899@gmail.com>
2016-04-15 17:28:08 -0400

    Use Ember.assign if available in preference to Ember.merge.

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -5,8 +5,12 @@ import config from '../../config/environment';
 export default function startApp(attrs) {
   let application;
 
-  let attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+  // use defaults, but you can override.
+  // Use assign, if available. TODO: Delete if/when ember-paper requires ember-2.5 or higher.
+  // jshint laxbreak: true
+  let attributes = Ember.assign
+                      ? Ember.assign({}, config.APP, attrs)
+                      : Ember.merge(Ember.merge({}, config.APP), attrs);
 
   Ember.run(() => {
     application = Application.create(attributes);
