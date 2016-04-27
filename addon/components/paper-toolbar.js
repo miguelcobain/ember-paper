b90e902f419c1a384c9d544c9c3ab47cd038d214
Andrew Hollandmoritz <ahollandmoritz@gmail.com>
2016-04-27 04:29:39 -0400

    Added 'tall' option, and example to docs for it and accent

--- addon/components/paper-toolbar.js
+++ addon/components/paper-toolbar.js
@@ -3,5 +3,7 @@ import ColorMixin from 'ember-paper/mixins/color-mixin';
 
 export default Ember.Component.extend(ColorMixin, {
   tagName: 'md-toolbar',
-  classNames: ['md-default-theme']
+  classNames: ['md-default-theme'],
+  classNameBindings: ['tall:md-tall','accent:md-accent'],
+  tall:false,
 });
