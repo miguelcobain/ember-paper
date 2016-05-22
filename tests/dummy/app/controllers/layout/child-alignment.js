bd23e0d79d8ab9ffb1628e38443a95e69b98b7d0
Jouke van der Maas <jouke.vandermaas@gmail.com>
2016-05-22 17:36:03 +0200

    Create documentation pages

---
+++ tests/dummy/app/controllers/layout/child-alignment.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+const { Controller, computed } = Ember;
+
+export default Controller.extend({
+  layoutClass: computed('layoutDirection', function() {
+    return `layout-${this.get('layoutDirection')}`;
+  }),
+
+  layoutAlignClass: computed('mainDirection', 'perpDirection', function() {
+    let { mainDirection, perpDirection } = this.getProperties('mainDirection', 'perpDirection');
+
+    if (!mainDirection && !perpDirection) {
+      return 'layout-align';
+    } else if (mainDirection && !perpDirection) {
+      return `layout-align-${mainDirection}`;
+    } else if (!mainDirection && perpDirection) {
+      return `layout-align-start-${perpDirection}`;
+    } else if (mainDirection && perpDirection) {
+      return `layout-align-${mainDirection}-${perpDirection}`;
+    }
+
+    return '';
+  }),
+
+  layoutDirection: 'row',
+  mainDirection: 'center',
+  perpDirection: 'center'
+});
