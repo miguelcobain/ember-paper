918348a1157dca04b04f557296b8b4c7b1b2c869
Miguel Andrade <jmandrade0@gmail.com>
2016-06-16 00:17:19 +0100

    customize positioning logic

--- addon/components/paper-menu-trigger.js
+++
@@ -1,5 +0,0 @@
-import BasicDropdownTriggerComponent from 'ember-basic-dropdown/components/basic-dropdown/trigger';
-
-export default BasicDropdownTriggerComponent.extend({
-  tagName: 'md-menu'
-});
--- addon/components/paper-menu.js
+++ addon/components/paper-menu.js
@@ -1,17 +1,133 @@
+import Ember from 'ember';
 import BasicDropdownComponent from 'ember-basic-dropdown/components/basic-dropdown';
+const { assert } = Ember;
+
+const MENU_EDGE_MARGIN = 8;
+
+function clamp(pos, bounds, containerNode) {
+  pos.top = Math.max(Math.min(pos.top, bounds.bottom - containerNode.offsetHeight), bounds.top);
+  pos.left = Math.max(Math.min(pos.left, bounds.right - containerNode.offsetWidth), bounds.left);
+}
+
+function firstVisibleChild(node) {
+  for (var i = 0; i < node.children.length; ++i) {
+    if (window.getComputedStyle(node.children[i]).display !== 'none') {
+      return node.children[i];
+    }
+  }
+}
 
 export default BasicDropdownComponent.extend({
-  renderInPlace: true
-  /*applyReposition(trigger, dropdown, positions) {
-    let { top: triggerTop } = trigger.getBoundingClientRect();
-    let $window = $(self.window);
-    let scroll = { top: $window.scrollTop() };
-    let triggerTopWithScroll = triggerTop + scroll.top;
-    let dropdownTop = triggerTopWithScroll;
+  positionMode: {
+    left: 'target',
+    top: 'target'
+  },
 
-    positions.style.top = `${dropdownTop}px`;
-    this._super(trigger, dropdown, positions);
-  }*/
+  performFullReposition(trigger, dropdown) {
+    let containerNode = dropdown;
+    let openMenuNode = dropdown.firstElementChild;
+    let openMenuNodeRect = openMenuNode.getBoundingClientRect();
+    let boundryNode = document.body;
+    let boundryNodeRect = boundryNode.getBoundingClientRect();
+
+    let menuStyle = window.getComputedStyle(openMenuNode);
+
+    let originNode = trigger.querySelector('.md-menu-origin') || trigger;
+    let originNodeRect = originNode.getBoundingClientRect();
+
+    let bounds = {
+      left: boundryNodeRect.left + MENU_EDGE_MARGIN,
+      top: Math.max(boundryNodeRect.top, 0) + MENU_EDGE_MARGIN,
+      bottom: Math.max(boundryNodeRect.bottom, Math.max(boundryNodeRect.top, 0) + boundryNodeRect.height) - MENU_EDGE_MARGIN,
+      right: boundryNodeRect.right - MENU_EDGE_MARGIN
+    };
+
+    let alignTarget;
+    let alignTargetRect = { top: 0, left: 0, right: 0, bottom: 0 };
+    let existingOffsets = { top: 0, left: 0, right: 0, bottom: 0 };
+    let positionMode = this.get('positionMode');
+
+    if (positionMode.top === 'target' || positionMode.left === 'target' || positionMode.left === 'target-right') {
+      alignTarget = firstVisibleChild(openMenuNode);
+      if (alignTarget) {
+        // TODO: Allow centering on an arbitrary node, for now center on first menu-item's child
+        alignTarget = alignTarget.firstElementChild || alignTarget;
+        alignTarget = alignTarget.querySelector('.md-menu-align-target') || alignTarget;
+        alignTargetRect = alignTarget.getBoundingClientRect();
+
+        existingOffsets = {
+          top: parseFloat(containerNode.style.top || 0),
+          left: parseFloat(containerNode.style.left || 0)
+        };
+      }
+    }
+
+    let position = {};
+    let transformOrigin = 'top ';
+
+    switch (positionMode.top) {
+      case 'target':
+        position.top = existingOffsets.top + originNodeRect.top - alignTargetRect.top;
+        break;
+      case 'cascade':
+        position.top = originNodeRect.top - parseFloat(menuStyle.paddingTop) - originNode.style.top;
+        break;
+      case 'bottom':
+        position.top = originNodeRect.top + originNodeRect.height;
+        break;
+      default:
+        assert(`Invalid target mode '${positionMode.top}' specified for paper-menu on Y axis.`);
+    }
+
+    switch (positionMode.left) {
+      case 'target':
+        position.left = existingOffsets.left + originNodeRect.left - alignTargetRect.left;
+        transformOrigin += 'left';
+        break;
+      case 'target-left':
+        position.left = originNodeRect.left;
+        transformOrigin += 'left';
+        break;
+      case 'target-right':
+        position.left = originNodeRect.right - openMenuNodeRect.width + (openMenuNodeRect.right - alignTargetRect.right);
+        transformOrigin += 'right';
+        break;
+      case 'cascade':
+        let willFitRight = (originNodeRect.right + openMenuNodeRect.width) < bounds.right;
+        position.left = willFitRight ? originNodeRect.right - originNode.style.left : originNodeRect.left - originNode.style.left - openMenuNodeRect.width;
+        transformOrigin += willFitRight ? 'left' : 'right';
+        break;
+      case 'right':
+        position.left = originNodeRect.right - openMenuNodeRect.width;
+        transformOrigin += 'right';
+        break;
+      case 'left':
+        position.left = originNodeRect.left;
+        transformOrigin += 'left';
+        break;
+      default:
+        assert(`Invalid target mode '${positionMode.left}' specified for paper-menu on X axis.`);
+    }
+
+    // TODO sum offsets
+    clamp(position, bounds, containerNode)
+
+    let dropdownTop = Math.round(position.top);
+    let dropdownLeft = Math.round(position.left);
+
+    let scaleX = Math.round(100 * Math.min(originNodeRect.width / containerNode.offsetWidth, 1.0)) / 100;
+    let scaleY = Math.round(100 * Math.min(originNodeRect.height / containerNode.offsetHeight, 1.0)) / 100;
+
+    let style = {
+      top: `${dropdownTop}px`,
+      left: `${dropdownLeft}px`,
+      // Animate a scale out if we aren't just repositioning
+      transform: !this.publicAPI.isOpen ? `scale(${scaleX}, ${scaleY})` : undefined,
+      transformOrigin
+    };
+
+    this.applyReposition(trigger, dropdown, { style });
+  }
 });
 
 /*
