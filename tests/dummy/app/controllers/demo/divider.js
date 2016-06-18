a1413845e04263a793e6ce88e6146f5feeeb9bdd
Andrew Hollandmoritz <ahollandmoritz@gmail.com>
2016-06-18 05:35:06 -0400

    Added divider demo

---
+++ tests/dummy/app/controllers/demo/divider.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  messages: [
+    {
+      face: 'tomster.png',
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands",
+    },
+    {
+      face: 'tomster.png',
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands",
+    },
+    {
+      face: 'tomster.png',
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands",
+    },
+    {
+      face: 'tomster.png',
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands",
+    }
+  ]
+});
