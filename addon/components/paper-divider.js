ef3bea5a8f084427f0e75c1660b5a59c6e62e9ff
Andrew Hollandmoritz <ahollandmoritz@gmail.com>
2016-06-18 05:34:51 -0400

    Explicitly defined inset property as null by default on paper-divider, removed the 'insetAttr' computed property in favor of binding directly with inset.

--- addon/components/paper-divider.js
+++ addon/components/paper-divider.js
@@ -2,13 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'md-divider',
-  attributeBindings: ['insetAttr:md-inset'],
+  attributeBindings: ['inset:md-inset'],
   classNames: ['paper-divider','md-default-theme'],
-  /*
-   * Not binding boolean values in Ember 1.8.1?
-   * https://github.com/emberjs/ember.js/issues/9595
-   */
-  insetAttr: Ember.computed('inset', function() {
-    return this.get('inset') ? 'md-inset' : null;
-  })
+  inset: null,
 });
