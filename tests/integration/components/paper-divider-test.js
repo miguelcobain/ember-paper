fc1ec33bd9df521e7b8029f124d1ac3162a2e7c2
Andrew Hollandmoritz <ahollandmoritz@gmail.com>
2016-06-18 05:50:36 -0400

    Integration test for paper-divider

---
+++ tests/integration/components/paper-divider-test.js
@@ -0,0 +1,39 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('paper-divider', 'Integration | Component | paper divider', {
+  integration: true
+});
+
+test('has required classes by default', function(assert) {
+  assert.expect(2);
+  this.render(hbs`
+    {{#paper-divider}}
+    {{/paper-divider}}
+  `);
+
+  assert.ok(this.$('md-divider').hasClass('paper-divider'));
+  assert.ok(this.$('md-divider').hasClass('md-default-theme'));
+});
+
+test('uses md-inset attribute when passed inset=true', function(assert) {
+  assert.expect(1);
+
+  this.render(hbs`
+    {{#paper-divider inset=true}}
+    {{/paper-divider}}
+  `);
+
+  assert.equal(this.$('md-divider').attr('md-inset'), 'true');
+});
+
+test('md-inset attribute is not present when inset is not passed', function(assert) {
+  assert.expect(1);
+
+  this.render(hbs`
+    {{#paper-divider}}
+    {{/paper-divider}}
+  `);
+  // Attribute should NOT be present in md-divider
+  assert.notOk(this.$('md-divider').attr('md-inset'));
+});
