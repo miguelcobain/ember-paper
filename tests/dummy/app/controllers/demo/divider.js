21fa87ef40ccd26f851901c3fe098484d184c4c7
Andrew Hollandmoritz <ahollandmoritz@gmail.com>
2016-06-18 06:40:01 -0400

    Add source code to demo

--- tests/dummy/app/controllers/demo/divider.js
+++ tests/dummy/app/controllers/demo/divider.js
@@ -1,30 +1,45 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  messages: [
-    {
-      face: 'tomster.png',
-      what: "Brunch this weekend?",
-      who: "Min Li Chan",
-      notes: "I'll be in your neighborhood doing errands",
+  showSourceCodeFull: false,
+  showSourceCodeInset: false,
+
+  messages: Ember.A([{
+    face: 'tomster.png',
+    who: 'tomster 1',
+    what: 'a message for you',
+    notes: 'this is a message for you about ember'
+  }, {
+    face: 'tomster.png',
+    who: 'tomster 2',
+    what: 'a message for you',
+    notes: 'this is a message for you about ember'
+  }, {
+    face: 'tomster.png',
+    who: 'tomster 3',
+    what: 'a message for you',
+    notes: 'this is a message for you about ember'
+  }, {
+    face: 'tomster.png',
+    who: 'tomster 4',
+    what: 'a message for you',
+    notes: 'this is a message for you about ember'
+  }, {
+    face: 'tomster.png',
+    who: 'tomster 5',
+    what: 'a message for you',
+    notes: 'this is a message for you about ember'
+  }]),
+
+  actions: {
+
+    toggleSourceCodeFull() {
+      this.toggleProperty('showSourceCodeFull');
     },
-    {
-      face: 'tomster.png',
-      what: "Brunch this weekend?",
-      who: "Min Li Chan",
-      notes: "I'll be in your neighborhood doing errands",
-    },
-    {
-      face: 'tomster.png',
-      what: "Brunch this weekend?",
-      who: "Min Li Chan",
-      notes: "I'll be in your neighborhood doing errands",
-    },
-    {
-      face: 'tomster.png',
-      what: "Brunch this weekend?",
-      who: "Min Li Chan",
-      notes: "I'll be in your neighborhood doing errands",
+
+    toggleSourceCodeInset() {
+      this.toggleProperty('showSourceCodeInset');
     }
-  ]
+
+  }
 });
