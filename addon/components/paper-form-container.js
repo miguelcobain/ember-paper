d3e5163a72980f85d9aeee2b78e4bca5ff321ecb
Jordan Harris <redsoxchamps2008@gmail.com>
2016-07-04 13:33:21 -0400

    Style fixes

--- addon/components/paper-form-container.js
+++ addon/components/paper-form-container.js
@@ -1,19 +1,23 @@
 import Ember from 'ember';
+const { Component } = Ember;
 
-export default Ember.Component.extend({
+export default Component.extend({
 	tagName: '',
 	attributeBindings: ['style'],
 	style: 'width:100%',
+	onChange: null,
 	customValidations: [],
 	hasValidation: Ember.computed('min', 'max', 'errorMessages', 'maxlength', 'required', 'customValidations', function() {
 		return this.get('min') || this.get('max') || this.get('errorMessages') || this.get('maxlength') || this.get('required') || this.get('customValidations');
 	}),
-	didUpdateAttrs: function(params) {
-		if (params.oldAttrs.isTouched === params.newAttrs.isTouched) return;
+	didUpdateAttrs(params) {
+		if (params.oldAttrs.isTouched === params.newAttrs.isTouched) {
+			return;
+		}
 		this.set('touched', this.get('isTouched'));
 	},
 	actions: {
-		callOnInvalid: function(status) {
+		callOnInvalid(status) {
 			this.get('onInvalid')(status);
 		}
 	}
--- addon/components/paper-form.js
+++ addon/components/paper-form.js
@@ -1,31 +1,36 @@
 import Ember from 'ember';
+const { Component } = Ember;
 
-export default Ember.Component.extend({
+export default Component.extend({
 	tagName: '',
 	classNames: [],
 	attributeBindings: ['style'],
 	style: 'width:100%',
 	numberOfInvalids: 0,
 	isValid: Ember.computed('numberOfInvalids', function() {
-		return this.get('numberOfInvalids') === 0 ? true : false;
+		return this.get('numberOfInvalids') === 0;
 	}),
 	isInvalid: Ember.computed('isValid', function() {
 		return !this.get('isValid');
 	}),
 	sendToParent: Ember.on('init', Ember.observer('isValid', function() {
-		if (!this.get('parentAction')) return;
+		if (!this.get('parentAction')) {
+			return;
+		}
 		this.get('parentAction')(this.get('isValid'));
 	})),
 	actions: {
-		onInvalid: function(status) {
+		onInvalid(status) {
 			if (status || status === null) {
 				this.set('numberOfInvalids', this.get('numberOfInvalids') + 1);
 			} else {
-				if (this.get('numberOfInvalids') === 0) return;
+				if (this.get('numberOfInvalids') === 0) {
+					return;
+				}
 				this.set('numberOfInvalids', this.get('numberOfInvalids') - 1);
 			}
 		},
-		submit: function() {
+		submit() {
 			if (this.get('isInvalid')) {
 				this.set('isTouched', false);
 				this.set('isTouched', true);
