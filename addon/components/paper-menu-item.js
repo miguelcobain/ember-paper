1166dc2230ec27b6314d6bd0d2bf0b676602e8a9
Ian Barrick <ianbarrick@gmail.com>
2016-07-06 00:08:20 -0400

    keyboard support for new paper-menu, made menu-item contextual component

--- addon/components/paper-menu-item.js
+++ addon/components/paper-menu-item.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
+import ChildMixin from 'ember-paper/mixins/child-mixin';
 
 const { Component } = Ember;
 
-export default Component.extend({
+export default Component.extend(ChildMixin, {
   tagName: 'md-menu-item',
+  disabled: false,
 
   actions: {
     handleClick(event) {
