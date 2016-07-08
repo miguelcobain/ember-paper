1a7277098c99eeefa30688250d70a7aaf90ae50a
ibarrick <IanBarrick@gmail.com>
2016-07-08 13:04:33 -0400

    Added keyboard support for paper-menu (#416)

--- addon/components/paper-menu-content-inner.js
+++ addon/components/paper-menu-content-inner.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import ParentMixin from 'ember-paper/mixins/parent-mixin';
-const { Component, inject } = Ember;
+const { Component, inject, computed, run } = Ember;
 
 export default Component.extend(ParentMixin, {
   tagName: 'md-menu-content',
@@ -8,6 +8,17 @@ export default Component.extend(ParentMixin, {
   classNameBindings: ['dense:md-dense'],
 
   constants: inject.service(),
+  enabledMenuItems: computed.filterBy('childComponents', 'disabled', false),
+
+  didInsertElement() {
+    run.later(() => {
+      let focusTarget = this.$().find('.md-menu-focus-target');
+      if (!focusTarget.length) {
+        focusTarget = this.get('enabledMenuItems.firstObject.element.firstElementChild');
+      }
+      focusTarget.focus();
+    });
+  },
 
   keyDown(ev) {
     switch (ev.which) {
@@ -17,29 +28,43 @@ export default Component.extend(ParentMixin, {
       case this.get('constants.KEYCODE.LEFT_ARROW'):
       case this.get('constants.KEYCODE.UP_ARROW'):
         ev.preventDefault();
-        this.select(-1);
+        this.focusMenuItem(ev, -1);
         break;
       case this.get('constants.KEYCODE.RIGHT_ARROW'):
       case this.get('constants.KEYCODE.DOWN_ARROW'):
         ev.preventDefault();
-        this.select(1);
+        this.focusMenuItem(ev, 1);
         break;
     }
   },
 
-  select(increment) {
-    let groupValue = this.get('groupValue');
-    let index = 0;
+  focusMenuItem(e, direction) {
+    let currentItem = this.$(e.target).closest('md-menu-item');
 
-    if (groupValue) {
-      index = this.get('childValues').indexOf(groupValue);
-      index += increment;
-      let length = this.get('childValues.length');
-      index = ((index % length) + length) % length;
+    let children = this.get('enabledMenuItems');
+    let items = children.map((child) => child.element);
+
+    let currentIndex = items.indexOf(currentItem[0]);
+
+    // Traverse through our elements in the specified direction (+/-1) and try to
+    // focus them until we find one that accepts focus
+    for (let i = currentIndex + direction; i >= 0 && i < items.length; i = i + direction) {
+      let focusTarget = items[i].firstElementChild || items[i];
+      let didFocus = this.attemptFocus(focusTarget);
+      if (didFocus) {
+        break;
+      }
     }
+  },
 
-    let childRadio = this.get('enabledChildRadios').objectAt(index);
-    childRadio.set('focused', true);
-    this.sendAction('onChange', childRadio.get('value'));
+  attemptFocus(el) {
+    if (el && el.getAttribute('tabindex') !== -1) {
+      el.focus();
+      if (document.activeElement === el) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 });
