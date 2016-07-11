141d7d9ccb2471238ac877ad9c8ad1a5735b10be
Ian Barrick <ianbarrick@gmail.com>
2016-07-10 22:12:47 -0400

    passes syntax checks

--- addon/components/paper-virtual-repeat.js
+++ addon/components/paper-virtual-repeat.js
@@ -6,87 +6,87 @@ const { computed, run, get, set, Handlebars, RSVP } = Ember;
 const EXTRA_ROW_PADDING = 3;
 
 export default VirtualEachComponent.extend({
-	tagName: 'md-virtual-repeat-container',
-	classNames: ['md-virtual-repeat-container'],
-	classNameBindings: ['horizontal:md-orient-horizontal'],
-	visibleItemsRaw: computed.mapBy('visibleItems','raw'),
-	
-	actions: {
-		onScroll(e) {
-			this.eventHandlers.scroll.call(this,e);
-		}
-	},
-	defaultAttrs: {
-		scrollTimeout: 30
-	},
-	calculateVisibleItems(positionIndex) {
-	    run(() => {
-	      const startAt = get(this, '_startAt');
-	      const scrolledAmount = this.get('horizontal') ? this.$('.md-virtual-repeat-scroller').scrollLeft() : this.$('.md-virtual-repeat-scroller').scrollTop();
-	      const visibleStart = isNaN(positionIndex) ? Math.floor(scrolledAmount / this.get('itemHeight')) : Math.max(positionIndex);
+  tagName: 'md-virtual-repeat-container',
+  classNames: ['md-virtual-repeat-container'],
+  classNameBindings: ['horizontal:md-orient-horizontal'],
+  visibleItemsRaw: computed.mapBy('visibleItems', 'raw'),
 
-	      if (visibleStart !== startAt) {
-	        set(this, '_startAt', visibleStart);
-	      }
-	    });
+  actions: {
+    onScroll(e) {
+      this.eventHandlers.scroll.call(this, e);
+    }
+  },
+  defaultAttrs: {
+    scrollTimeout: 30
+  },
+  calculateVisibleItems(positionIndex) {
+    run(() => {
+      let startAt = get(this, '_startAt');
+      let scrolledAmount = this.get('horizontal') ? this.$('.md-virtual-repeat-scroller').scrollLeft() : this.$('.md-virtual-repeat-scroller').scrollTop();
+      let visibleStart = isNaN(positionIndex) ? Math.floor(scrolledAmount / this.get('itemHeight')) : Math.max(positionIndex);
+
+      if (visibleStart !== startAt) {
+        set(this, '_startAt', visibleStart);
+      }
+    });
   },
   _marginTop: computed('_totalHeight', '_startAt', '_visibleItemCount', 'itemHeight', {
     get() {
-      const itemHeight = this.get('itemHeight');
-      const totalHeight = get(this, '_totalHeight');
-      const margin = get(this, '_startAt') * itemHeight;
-      const visibleItemCount = get(this, '_visibleItemCount');
-      const maxMargin = Math.max(0, totalHeight - ((visibleItemCount - 1) * itemHeight) + (EXTRA_ROW_PADDING * itemHeight));
+      let itemHeight = this.get('itemHeight');
+      let totalHeight = get(this, '_totalHeight');
+      let margin = get(this, '_startAt') * itemHeight;
+      let visibleItemCount = get(this, '_visibleItemCount');
+      let maxMargin = Math.max(0, totalHeight - ((visibleItemCount - 1) * itemHeight) + (EXTRA_ROW_PADDING * itemHeight));
 
       return Math.min(maxMargin, margin);
     }
   }).readOnly(),
   contentStyle: computed('_marginTop', '_totalHeight', {
     get() {
-      
-      const height = Handlebars.Utils.escapeExpression(get(this, '_totalHeight'));
+
+      let height = Handlebars.Utils.escapeExpression(get(this, '_totalHeight'));
 
       return new Handlebars.SafeString(this.get('horizontal') ? `width: ${height}px;` : `height: ${height}px;`);
     }
   }).readOnly(),
   _visibleItemCount: computed('height', 'itemHeight', {
     get() {
-      const height = this.get('height');
+      let height = this.get('height');
 
       return Math.ceil(this.get('itemHeight') ? height / this.get('itemHeight') : 1) + EXTRA_ROW_PADDING;
     }
   }).readOnly(),
   didRender() {
-  	if (!this.get('itemHeight')) {
-  		let elem = this.$('.md-virtual-repeat-offsetter')[0].firstElementChild;
-  		if (elem) {
-  			this.set('itemHeight',this.get('horizontal')? elem.offsetWidth : elem.offsetHeight);
-  			this.set('_totalHeight',Math.max((this.get('length') ? this.get('length') : get(this.get('items'), 'length')) * this.get('itemHeight'), 0))
-  			// this.rerender();
-  		}
-  	}
-  	this.set('height',this.get('horizontal') ? this.$()[0].clientWidth : this.$()[0].clientHeight);
-  	
+    if (!this.get('itemHeight')) {
+      let elem = this.$('.md-virtual-repeat-offsetter')[0].firstElementChild;
+      if (elem) {
+        this.set('itemHeight', this.get('horizontal') ? elem.offsetWidth : elem.offsetHeight);
+        this.set('_totalHeight', Math.max((this.get('length') ? this.get('length') : get(this.get('items'), 'length')) * this.get('itemHeight'), 0));
+        // this.rerender();
+      }
+    }
+    this.set('height', this.get('horizontal') ? this.$()[0].clientWidth : this.$()[0].clientHeight);
+
   },
   visibleItems: computed('_startAt', '_visibleItemCount', '_items', {
     get() {
       let items = get(this, '_items');
-      const startAt = get(this, '_startAt');
-      const _visibleItemCount = get(this, '_visibleItemCount');
-      const itemsLength = this.get('length') ? this.get('length') : get(items, 'length');
-      const endAt = Math.min(itemsLength, startAt + _visibleItemCount);
-      const onScrollBottomed = this.attrs.onScrollBottomed;
+      let startAt = get(this, '_startAt');
+      let _visibleItemCount = get(this, '_visibleItemCount');
+      let itemsLength = this.get('length') ? this.get('length') : get(items, 'length');
+      let endAt = Math.min(itemsLength, startAt + _visibleItemCount);
+      let onScrollBottomed = this.getAttr('onScrollBottomed');
 
       if (typeof onScrollBottomed === 'function' && (startAt + _visibleItemCount - EXTRA_ROW_PADDING) >= itemsLength) {
         onScrollBottomed(startAt, endAt);
       }
       let getAtIndex = this.get('getAtIndex');
       if (getAtIndex) {
-      	for (let i = startAt; i < endAt; i++) {
-      		if (!items[i]) {
-      			items[i] = getAtIndex(i);
-      		}
-      	}
+        for (let i = startAt; i < endAt; i++) {
+          if (!items[i]) {
+            items[i] = getAtIndex(i);
+          }
+        }
       }
       return items.slice(startAt, endAt).map((item, index) => {
         return {
@@ -101,7 +101,7 @@ export default VirtualEachComponent.extend({
     this._super(...arguments);
 
     RSVP.cast(this.getAttr('items')).then((attrItems) => {
-      const items = Ember.A(attrItems);
+      let items = Ember.A(attrItems);
 
       this.setProperties({
         _items: items,
@@ -113,26 +113,26 @@ export default VirtualEachComponent.extend({
   scrollTo: Ember.observer('_positionIndex', function() {
 
     this.scheduledRender = run.scheduleOnce('afterRender', () => {
-      const positionIndex = get(this, '_positionIndex');
-      const itemHeight = this.get('itemHeight');
-      const totalHeight = get(this, '_totalHeight');
-      const _visibleItemCount = get(this, '_visibleItemCount');
-      const startingIndex = isNaN(positionIndex) ? get(this, '_startAt') : Math.max(positionIndex, 0);
-      const startingPadding = itemHeight * startingIndex;
-      const maxVisibleItemTop = Math.max(0, (get(this, '_items.length') - _visibleItemCount + EXTRA_ROW_PADDING));
-      const maxPadding = Math.max(0, totalHeight - ((_visibleItemCount - 1) * itemHeight) + (EXTRA_ROW_PADDING * itemHeight));
-      const sanitizedIndex = Math.min(startingIndex, maxVisibleItemTop);
-      const sanitizedPadding = (startingPadding > maxPadding) ? maxPadding : startingPadding;
+      let positionIndex = get(this, '_positionIndex');
+      let itemHeight = this.get('itemHeight');
+      let totalHeight = get(this, '_totalHeight');
+      let _visibleItemCount = get(this, '_visibleItemCount');
+      let startingIndex = isNaN(positionIndex) ? get(this, '_startAt') : Math.max(positionIndex, 0);
+      let startingPadding = itemHeight * startingIndex;
+      let maxVisibleItemTop = Math.max(0, (get(this, '_items.length') - _visibleItemCount + EXTRA_ROW_PADDING));
+      let maxPadding = Math.max(0, totalHeight - ((_visibleItemCount - 1) * itemHeight) + (EXTRA_ROW_PADDING * itemHeight));
+      let sanitizedIndex = Math.min(startingIndex, maxVisibleItemTop);
+      let sanitizedPadding = (startingPadding > maxPadding) ? maxPadding : startingPadding;
       this.calculateVisibleItems(sanitizedIndex);
-      if(this.get('horizontal')) {
-      	this.$('.md-virtual-repeat-scroller').scrollLeft(sanitizedPadding);
+      if (this.get('horizontal')) {
+        this.$('.md-virtual-repeat-scroller').scrollLeft(sanitizedPadding);
       } else {
-      	this.$('.md-virtual-repeat-scroller').scrollTop(sanitizedPadding);
+        this.$('.md-virtual-repeat-scroller').scrollTop(sanitizedPadding);
       }
     });
   }),
-  lengthObserver: Ember.observer('items.length',function() {
-    this.set('_totalHeight', Math.max((this.get('length') ? this.get('length') :this.get('items.length')) * this.get('itemHeight'), 0));
+  lengthObserver: Ember.observer('items.length', function() {
+    this.set('_totalHeight', Math.max((this.get('length') ? this.get('length') : this.get('items.length')) * this.get('itemHeight'), 0));
   })
 
 });
--- tests/dummy/app/controllers/demo/virtual-repeat.js
+++ tests/dummy/app/controllers/demo/virtual-repeat.js
@@ -3,86 +3,80 @@ import Ember from 'ember';
 const { computed, Controller, RSVP, run } = Ember;
 
 export default Controller.extend({
-   items: Ember.computed(function() {
-		let arr = [];
-		for (let i = 0; i < 1000; i++) {
-			arr.push(i);
-		}
-		return Ember.A(arr);
-   }),
-   items2: [],
-   items3: Ember.computed(function() {
-   	let selectedYear = 0;
+  items: computed(function() {
+    let arr = [];
+    for (let i = 0; i < 1000; i++) {
+      arr.push(i);
+    }
+    return Ember.A(arr);
+  }),
+  items2: [],
+  items3: computed(function() {
     let years = [];
     let items = [];
-    var currentYear = new Date().getFullYear();
-    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+    let currentYear = new Date().getFullYear();
+    let monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
       'July', 'August', 'September', 'October', 'November', 'December'];
     // Build a list of months over 20 years
-    for (var y = currentYear; y >= (currentYear-20); y--) {
+    for (let y = currentYear; y >= (currentYear - 20); y--) {
       years.push(y);
-      items.push({year: y, text: y, header: true});
-      for (var m = 11; m >= 0; m--) {
-        items.push({year: y, month: m, text: monthNames[m]});
+      items.push({ year: y, text: y, header: true });
+      for (let m = 11; m >= 0; m--) {
+        items.push({ year: y, month: m, text: monthNames[m] });
       }
     }
-    this.set('years',years);
-    return Ember.Object.create({items: items, years: Ember.A(years)});
-   }),
-   infiniteItems: Ember.A([]),
-   loadedPages: {},
-   PAGE_SIZE: 50,
-   scrollIndex: 0,
-   yearIndex: 0,
-   length: 50000,
-   actions: {
+    this.set('years', years);
+    return items;
+  }),
+  infiniteItems: Ember.A([]),
+  loadedPages: {},
+  PAGE_SIZE: 50,
+  scrollIndex: 0,
+  yearIndex: 0,
+  length: 50000,
+  actions: {
     fetchMore() {
       let arr = this.get('infiniteItems');
-     
+
       let length = arr.get('length');
       for (let i = length; i < length + 50; i++) {
         arr.pushObject(i);
       }
-      
-      //arr.push(arr1[arr.length]);
     },
-   	scrollTo(selected) {
-   		this.set('selected',selected);
-   		
-   		this.set('scrollIndex',this.get('years').indexOf(selected) * 13);
-   		
-   	},
-   	getAtIndex(index) {
-   		let pageNumber = Math.floor(index / this.PAGE_SIZE);
-   		let page = this.loadedPages[pageNumber];
-   		if (page) {
-   			return new RSVP.Promise((resolve,reject) => resolve(page[index % this.PAGE_SIZE]));
-   		} else if (page !== null) {
-   			return this.fetchPage(pageNumber,index)
-   		} else {
-   			return new RSVP.Promise((resolve,reject) => {
-   				run.later(this,function() {
-   					let pageOffset = pageNumber * this.PAGE_SIZE;
-   					console.log(this.loadedPages[pageNumber][index - pageOffset]);
-   					resolve(this.loadedPages[pageNumber][index - pageOffset]);
-   				},300);
-   			});
-   		}
-   		
-   	}
-   },
-   fetchPage(pageNumber,index) {
-   	this.loadedPages[pageNumber] = null;
-   	return new RSVP.Promise((resolve,reject) => {
-   		run.later(this,function() {
-	   		this.loadedPages[pageNumber] = [];
-	   		let pageOffset = pageNumber * this.PAGE_SIZE;
-	   		for (let i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
-	   			this.loadedPages[pageNumber].push(i);
-	   		}
-	   		console.log(this.loadedPages[pageNumber][index]);
-	   		resolve(this.loadedPages[pageNumber][index-pageOffset]);
-   		},300);
-   	});
-   }
-})
+    scrollTo(selected) {
+      this.set('selected', selected);
+
+      this.set('scrollIndex', this.get('years').indexOf(selected) * 13);
+
+    },
+    getAtIndex(index) {
+      let pageNumber = Math.floor(index / this.PAGE_SIZE);
+      let page = this.loadedPages[pageNumber];
+      if (page) {
+        return new RSVP.Promise((resolve) => resolve(page[index % this.PAGE_SIZE]));
+      } else if (page !== null) {
+        return this.fetchPage(pageNumber, index);
+      } else {
+        return new RSVP.Promise((resolve) => {
+          run.later(this, function() {
+            let pageOffset = pageNumber * this.PAGE_SIZE;
+            resolve(this.loadedPages[pageNumber][index - pageOffset]);
+          }, 300);
+        });
+      }
+    }
+  },
+  fetchPage(pageNumber, index) {
+    this.loadedPages[pageNumber] = null;
+    return new RSVP.Promise((resolve) => {
+      run.later(this, function() {
+        this.loadedPages[pageNumber] = [];
+        let pageOffset = pageNumber * this.PAGE_SIZE;
+        for (let i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
+          this.loadedPages[pageNumber].push(i);
+        }
+        resolve(this.loadedPages[pageNumber][index - pageOffset]);
+      }, 300);
+    });
+  }
+});
