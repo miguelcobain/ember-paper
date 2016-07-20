c7ff1cdf3b726b00fa8c38b9d764c29d0a5f97f2
Robert Wagner <rwwagner90@gmail.com>
2016-07-20 12:20:29 -0400

    Make ember-cli-sass a dependency

--- blueprints/ember-paper/index.js
+++ blueprints/ember-paper/index.js
@@ -8,14 +8,9 @@ module.exports = {
   },
 
   afterInstall: function() {
-    var _this = this;
     return this.addBowerPackagesToProject([
-      {name: 'hammer.js', target:'latest'},
-      {name: 'matchMedia', target: '0.2.0'}
-    ]).then(function() {
-      return _this.addPackagesToProject([
-        {name: 'ember-cli-sass', target: 'latest'}
-      ]);
-    });
+      { name: 'hammer.js', target: 'latest' },
+      { name: 'matchMedia', target: '0.2.0' }
+    ]);
   }
 };
