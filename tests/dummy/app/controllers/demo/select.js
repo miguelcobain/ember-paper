cabde7f40be93e40be80ec86c892793063ae7e41
Ian Barrick <ianbarrick@gmail.com>
2016-07-28 17:43:28 -0400

    resolved merge difficulties

--- tests/dummy/app/controllers/demo/select.js
+++ tests/dummy/app/controllers/demo/select.js
@@ -9,19 +9,7 @@ export default Controller.extend({
     return 'AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY'
             .split(' ').map((state) => ({ abbrev: state }));
   }),
-  vegetables: Ember.A([
-    { name: 'Corn', checked: false },
-    { name: 'Onions', checked: false },
-    { name: 'Kale', checked: false },
-    { name: 'Arugula', checked: false },
-    { name: 'Peas', checked: false },
-    { name: 'Zucchini', checked: false }]),
-  searchTerm: '',
-  filteredVegetables: computed('vegetables.[]', 'searchTerm', function() {
-    return this.get('vegetables').filter((vegetable) => {
-      return vegetable.name.toLowerCase().indexOf(this.get('searchTerm').toLowerCase()) > -1;
-    });
-  }),
+
   sizes: Ember.A([
     'small (12-inch)',
     'medium (14-inch)',
@@ -62,33 +50,8 @@ export default Controller.extend({
       }, waitMS);
 
     });
-  },
-
-  userLabelCallback(item) {
-    // using ember data, this might be 'item.get('name')'
-    return item.name;
-  },
-  vegetableLabelCallback(item) {
-    return item.name;
-  },
-  toppings: Ember.A([
-    { category: 'meat', name: 'Pepperoni' },
-    { category: 'meat', name: 'Sausage' },
-    { category: 'meat', name: 'Ground Beef' },
-    { category: 'meat', name: 'Bacon' },
-    { category: 'veg', name: 'Mushrooms' },
-    { category: 'veg', name: 'Onion' },
-    { category: 'veg', name: 'Green Pepper' },
-    { category: 'veg', name: 'Green Olives' }
-  ]),
-
-  meatToppings: computed.filterBy('toppings', 'category', 'meat'),
-
-  vegToppings: computed.filterBy('toppings', 'category', 'veg'),
-  actions: {
-    searchKeyPressed(e) {
-      e.stopPropagation();
-    }
-  }
-
+  }),
+  groupedToppings: [
+    { groupName: 'Meats', options: ['Pepperoni', 'Sausage', 'Ground Beef', 'Bacon'] },
+    { groupName: 'Veg', options: ['Mushrooms', 'Onion', 'Green Pepper', 'Green Olives'] } ]
 });
