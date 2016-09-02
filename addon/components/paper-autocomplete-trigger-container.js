2c88c44f62bb5861917bf889795af56de09d3943
Andrey Khomenko <andrey.khomenko@siemens.com>
2016-09-02 01:46:45 -0400

    Fixed paper-autocomplete deprecations

--- addon/components/paper-autocomplete-trigger-container.js
+++ addon/components/paper-autocomplete-trigger-container.js
@@ -4,9 +4,22 @@ import BasicTrigger from 'ember-basic-dropdown/components/basic-dropdown/trigger
 const { computed } = Ember;
 
 export default BasicTrigger.extend({
-  attributeBindings: ['label:md-floating-label','disabled:disabled'],
-  classNameBindings: ['flex'],
+  tagName: 'md-autocomplete',
+  attributeBindings: ['label:md-floating-label','disabled:disabled', 'flex'],
   disabled: computed('disabledProxy', function() {
     return this.get('disabledProxy') ? this.get('disabledProxy') : undefined;
-  })
+  }),
+
+  // Chrome 51: setting tabindex=0 explicitly stops tab propogation to
+  // other elements. We need to verify
+  tabIndex: computed('dropdown.disabled', 'tabindex', function() {
+   let tabindex = this.get('tabindex');
+
+   //tabindex = false|0 - don't set tabindex attr
+   if (!tabindex || this.get('dropdown.disabled')) {
+     return null;
+   }
+   return tabindex;
+  }),
+
 });
