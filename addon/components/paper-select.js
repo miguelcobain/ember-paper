9c2d5899625534567fb443b716a362a2279760a1
Ian Barrick <ianbarrick@gmail.com>
2016-09-09 13:07:06 -0400

    extracted validation to a mixin and added the mixin to paper-select

--- addon/components/paper-select.js
+++ addon/components/paper-select.js
@@ -3,18 +3,61 @@
  */
 import Ember from 'ember';
 import PowerSelect from 'ember-power-select/components/power-select';
+import ValidationMixin from 'ember-paper/mixins/validation-mixin';
 
 const { computed } = Ember;
+
+function concatWithProperty(strings, property) {
+  if (property) {
+    strings.push(property);
+  }
+  return strings.join(' ');
+}
 
 /**
  * @class PaperSelect
  * @extends PaperInput
  */
-export default PowerSelect.extend({
+export default PowerSelect.extend(ValidationMixin, {
   tagName: 'md-input-container',
   onchange: computed.alias('onChange'),
   optionsComponent: 'paper-select-options',
   triggerComponent: 'paper-select-trigger',
   beforeOptionsComponent: 'paper-select-search',
-  searchEnabled: false
+  classNameBindings: ['isInvalidAndTouched:md-input-invalid'],
+  searchEnabled: false,
+  validationProperty: 'selected',
+  isTouched: false,
+  isInvalidAndTouched: computed.and('isInvalid', 'isTouched'),
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.notifyValidityChange();
+  },
+  willClearRender() {
+    this.sendAction('onValidityChange', false);
+  },
+  concatenatedTriggerClasses: computed('triggerClass', 'publicAPI.isActive', function() {
+    let classes = ['ember-power-select-trigger'];
+    if (this.get('isInvalid')) {
+      classes.push('ng-invalid');
+    }
+    if (this.get('isTouched')) {
+      classes.push('ng-dirty');
+    }
+    if (this.get('publicAPI.isActive')) {
+      classes.push('ember-power-select-trigger--active');
+    }
+    return concatWithProperty(classes, this.get('triggerClass'));
+  }),
+  actions: {
+    onClose() {
+      this._super(...arguments);
+      this.set('isTouched', true);
+      this.notifyValidityChange();
+    },
+    onOpen() {
+      this._super(...arguments);
+      this.notifyValidityChange();
+    }
+  }
 });
