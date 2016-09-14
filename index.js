72f50754d850c0f1aafdbaf2fcbc455b9f17aa83
Paul Nicholls <paul.n@linc-ed.com>
2016-09-14 14:02:46 +1200

    Call power-select's contentFor hook for body-footer
    Without this, attempting to use paper-autocomplete results in:
    ember-wormhole failed to render into '#ember-basic-dropdown-wormhole'
    because the element is not in the DOM

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ module.exports = {
     if (type === 'head') {
       return '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">' +
         '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
+    } else if (type === 'body-footer') {
+      var emberPowerSelect = this.addons.filter(function(addon) {
+        return addon.name === 'ember-power-select';
+      })[0];
+      return emberPowerSelect.contentFor(type, config);
     }
   },
 
