311fa4bb57574e64d7567ceb3f05a6d9db14807f
Miguel Andrade <jmandrade0@gmail.com>
2016-09-20 22:31:34 +0100

    remove unecessary attribute bindings

--- addon/components/paper-select-menu-trigger.js
+++ addon/components/paper-select-menu-trigger.js
@@ -5,22 +5,8 @@ const { computed } = Ember;
 
 export default BasicTrigger.extend({
   tagName: 'md-select',
-  attributeBindings: [
-    'role',
-    'tabIndex:tabindex',
-    'dropdownId:aria-controls',
-    'ariaLabel:aria-label',
-    'ariaLabelledBy:aria-labelledby',
-    'ariaDescribedBy:aria-describedby',
-    'aria-disabled',
-    'aria-expanded',
-    'aria-haspopup',
-    'aria-invalid',
-    'aria-pressed',
-    'aria-required',
-    'disabled:disabled'
-  ],
-  disabled: computed('disabledProxy', function() {
-    return this.get('disabledProxy') ? this.get('disabledProxy') : undefined;
-  }),
+  attributeBindings: ['disabledAttr:disabled'],
+  disabledAttr: computed('disabled', function() {
+    return this.get('disabled') ? 'disabled' : null;
+  })
 });
