26dbe4d96a90f9cb5eebd78dea89f90053ac78c9
Paul Nicholls <paul.n@linc-ed.com>
2016-09-20 15:49:59 +1200

    Make paper-contact-chips work with new paper-autocomplete (based on ember-power-select)

--- addon/components/paper-contact-chips.js
+++ addon/components/paper-contact-chips.js
@@ -13,6 +13,33 @@ export default Component.extend({
 
     inputBlur() {
       this.set('isFocused', false);
+    },
+
+    autocompleteChange(item, select) {
+      if (item) {
+        // Trigger onChange for the new item.
+        this.sendAction('addItem', item);
+
+        // Reset the underlying ember-power-select so that it's ready for another selection.
+        this.$('.ember-power-select-typeahead-input', this.element)[0].value = '';
+        select.actions.search('');
+
+        // Re-open ember-power-select to trigger it to reposition the dropdown.
+        select.actions.close();
+        select.actions.open();
+      }
+    },
+
+    autocompleteClose() {
+      if (this.get('source').length) {
+        // We still have options left; go ahead and close the underlying ember-power-select.
+        return true;
+      }
+
+      // There are no options left; the paper-autocomplete / ember-power-select will be removed
+      // automatically, so there's no need to try closing it - if you do, you risk trying to close
+      // it after it's already been destroyed, which throws an error.
+      return false;
     }
   }
 });
