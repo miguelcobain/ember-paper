0c88abf5689c7e5837f984bc27ace7eefe9e1fac
Paul Nicholls <paul.n@linc-ed.com>
2016-09-20 13:09:03 +1200

    Fix misplaced comma from my sloppy merge

--- index.js
+++ index.js
@@ -109,10 +109,10 @@ module.exports = {
       'components/dialog/dialog.scss',
       'components/dialog/dialog-theme.scss',
 
-      'components/virtualRepeat/virtual-repeater.scss'
+      'components/virtualRepeat/virtual-repeater.scss',
 
       'components/chips/chips.scss',
-      'components/chips/chips-theme.scss',
+      'components/chips/chips-theme.scss'
     ];
 
     /*
