bb8d4a2a85311527b2e8c1e90b897732964610de
Paul Nicholls <paul.n@linc-ed.com>
2016-09-21 15:15:36 +1200

    Refactor keyboard navigation in chips to make it easier for contact chips to extend

--- addon/components/paper-chips.js
+++ addon/components/paper-chips.js
@@ -38,37 +38,41 @@ export default Component.extend({
 
     keyDown(event) {
       if (!this.get('readOnly') && isEmpty(this.get('newChipValue')) && isPresent(this.get('content'))) {
-        // No text has been entered, but we have chips; cursor keys should select chips.
-        const key = event.key,
-          current = this.get('activeChip'),
-          chips = this.get('content'),
-          input = this.$('.md-chip-input-container input');
-
-        if (key === 'ArrowLeft' || (key === 'Backspace' && current === -1)) {
-          if (current === -1) {
-            this.$('md-chips-wrap', this.element).focus();
-            this.set('activeChip', chips.length-1);
-          } else if (current > 0) {
-            this.decrementProperty('activeChip');
-          }
-        } else if (key === 'ArrowRight') {
-          if (current >= 0) {
-            this.incrementProperty('activeChip');
-          }
-
-          if (this.get('activeChip') >= chips.length) {
-            this.set('activeChip', -1);
-            input.focus();
-          }
-        } else if (current >= 0 && (key === 'Delete' || key === 'Backspace')) {
-          this.sendAction('removeItem', chips[current]);
-          if (current >= chips.length) {
-            input.focus();
-            this.set('activeChip', -1);
-          }
-        }
+        this.keyboardNavigation(event);
       } else {
         // Make sure we don't leave a chip focused while typing.
+        this.set('activeChip', -1);
+      }
+    }
+  },
+
+  keyboardNavigation(event) {
+    // No text has been entered, but we have chips; cursor keys should select chips.
+    const key = event.key,
+      current = this.get('activeChip'),
+      chips = this.get('content'),
+      input = this.$('.md-chip-input-container input');
+
+    if (key === 'ArrowLeft' || (key === 'Backspace' && current === -1)) {
+      if (current === -1) {
+        this.$('md-chips-wrap', this.element).focus();
+        this.set('activeChip', chips.length-1);
+      } else if (current > 0) {
+        this.decrementProperty('activeChip');
+      }
+    } else if (key === 'ArrowRight') {
+      if (current >= 0) {
+        this.incrementProperty('activeChip');
+      }
+
+      if (this.get('activeChip') >= chips.length) {
+        this.set('activeChip', -1);
+        input.focus();
+      }
+    } else if (current >= 0 && (key === 'Delete' || key === 'Backspace')) {
+      this.sendAction('removeItem', chips[current]);
+      if (current >= chips.length) {
+        input.focus();
         this.set('activeChip', -1);
       }
     }
