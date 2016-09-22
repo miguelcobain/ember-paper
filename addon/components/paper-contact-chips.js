2bee907cdd42ddc85a6b8902028f0e5f622040e7
Paul Nicholls <paul.n@linc-ed.com>
2016-09-22 14:47:53 +1200

    JSHint / JSCS compliance for chips

--- addon/components/paper-contact-chips.js
+++ addon/components/paper-contact-chips.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 import PaperChips from 'ember-paper/components/paper-chips';
 
-let { isEmpty, isPresent } = Ember;
-
 export default PaperChips.extend({
   tagName: 'md-contact-chips',
   classNames: ['md-default-theme']
