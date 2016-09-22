8871f7353631fb6359434ce81af9c0f51a30d0af
Paul Nicholls <paul.n@linc-ed.com>
2016-09-22 14:23:51 +1200

    Add autocomplete demos for regular (non-contact) chips

--- tests/dummy/app/controllers/demo/chips.js
+++ tests/dummy/app/controllers/demo/chips.js
@@ -29,6 +29,48 @@ export default Ember.Controller.extend({
     });
   }),
 
+  vegetables: Ember.A([{
+    name: 'Broccoli',
+    family: 'Brassica'
+  }]),
+
+  allVegetables: Ember.A([{
+    name: 'Broccoli',
+    family: 'Brassica'
+  }, {
+    name: 'Cabbage',
+    family: 'Brassica'
+  }, {
+    name: 'Carrot',
+    family: 'Umbelliferous'
+  }, {
+    name: 'Lettuce',
+    family: 'Composite'
+  }, {
+    name: 'Spinach',
+    family: 'Goosefoot'
+  }]),
+
+  remainingVegetables: Ember.computed('allVegetables.@each.name', 'vegetables.@each.name', function() {
+    return this.get('allVegetables').filter((source) => {
+      return !this.get('vegetables').any(function(myVegetable) {
+        return source.name === myVegetable.name;
+      });
+    });
+  }),
+
+  vegeNames: Ember.A(['Broccoli']),
+
+  allVegeNames: Ember.A(['Broccoli', 'Cabbage', 'Carrot', 'Lettuce', 'Spinach']),
+
+  remainingVegeNames: Ember.computed('vegeNames.length', function() {
+    return this.get('allVegeNames').filter((source) => {
+      return !this.get('vegeNames').any(function(myVegeName) {
+        return source === myVegeName;
+      });
+    });
+  }),
+
   actions: {
     removeItem(item) {
       this.get('fruitNames').removeObject(item);
@@ -44,6 +86,22 @@ export default Ember.Controller.extend({
 
     addCustomItem(item) {
       this.get('customFruitNames').pushObject(item);
+    },
+
+    removeVegetable(item) {
+      this.get('vegetables').removeObject(item);
+    },
+
+    addVegetable(item) {
+      this.get('vegetables').pushObject(item);
+    },
+
+    removeVegeName(item) {
+      this.get('vegeNames').removeObject(item);
+    },
+
+    addVegeName(item) {
+      this.get('vegeNames').pushObject(item);
     },
 
     addContact(item) {
