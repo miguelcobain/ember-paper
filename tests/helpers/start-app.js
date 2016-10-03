e90f7de5720778c61aeab5be0ca79e38867107d9
Miguel Andrade <jmandrade0@gmail.com>
2016-10-03 14:03:27 +0100

    update ember-cli to 2.8.0

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -5,12 +5,8 @@ import config from '../../config/environment';
 export default function startApp(attrs) {
   let application;
 
-  // use defaults, but you can override.
-  // Use assign, if available. TODO: Delete if/when ember-paper requires ember-2.5 or higher.
-  // jshint laxbreak: true
-  let attributes = Ember.assign
-                      ? Ember.assign({}, config.APP, attrs)
-                      : Ember.merge(Ember.merge({}, config.APP), attrs);
+  let attributes = Ember.merge({}, config.APP);
+  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
   Ember.run(() => {
     application = Application.create(attributes);
