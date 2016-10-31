f283db7115db8056fc8d2c6069bc400c3a01e27a
Jordan Harris <redsoxchamps2008@gmail.com>
2016-10-31 16:13:25 -0400

    Finished form page

--- tests/dummy/app/controllers/demo/form.js
+++ tests/dummy/app/controllers/demo/form.js
@@ -1,13 +1,22 @@
 import Ember from 'ember';
 const { Controller, run } = Ember;
 
-export default Controller.extend({
+export default Controller.extend({	
   actions: {
-    mySubmitAction(e) {
-      this.set('basicUsageSuccess', true);
-      run.later(this, () => {
-        this.set('basicUsageSuccess', false);
-      }, 4000);
+    basicSubmitAction() {
+      // resetting our form's state
+      this.set('firstName', '');
+      this.set('middleInitial', '');
+      this.set('lastName', '');
+      this.set('age', '');
+    },
+    disabledSubmitAction() {
+	  // resetting our form's state
+      this.set('favoriteLetter', '');
+    },
+    customSubmitAction() {
+      // resetting our form's state
+      this.set('favoriteNumber', '');
     }
   }
 });
