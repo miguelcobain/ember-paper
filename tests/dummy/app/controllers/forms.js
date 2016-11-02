3ab07528608ce52bbc4d1b5cbf6bc4991c6c201d
Miguel Andrade <jmandrade0@gmail.com>
2016-11-02 15:26:48 +0000

    add some details to forms documentation

--- tests/dummy/app/controllers/forms.js
+++ tests/dummy/app/controllers/forms.js
@@ -1,13 +1,26 @@
 import Ember from 'ember';
-const { Controller } = Ember;
+const { Controller, A } = Ember;
 
 export default Controller.extend({
+  items: A([
+    { name: 'Afghanistan', code: 'AF' },
+    { name: 'Åland Islands', code: 'AX' },
+    { name: 'Albania', code: 'AL' },
+    { name: 'Algeria', code: 'DZ' },
+    { name: 'American Samoa', code: 'AS' },
+    { name: 'AndorrA', code: 'AD' },
+    { name: 'Angola', code: 'AO' },
+    { name: 'Anguilla', code: 'AI' },
+    { name: 'Antarctica', code: 'AQ' },
+    { name: 'Antigua and Barbuda', code: 'AG' }
+  ]),
+
   actions: {
     basicSubmitAction() {
       this.set('firstName', '');
-      this.set('middleInitial', '');
       this.set('lastName', '');
       this.set('age', '');
+      this.set('selectedCountry', null);
     },
     disabledSubmitAction() {
       this.set('favoriteLetter', '');
