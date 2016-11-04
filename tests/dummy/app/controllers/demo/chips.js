962e955ed176081cf058ac81681fc064041e574c
Paul Nicholls <paul.n@linc-ed.com>
2016-11-04 16:54:44 +1300

    Add demo for newly-configurable fields in paper-contact-chips

--- tests/dummy/app/controllers/demo/chips.js
+++ tests/dummy/app/controllers/demo/chips.js
@@ -37,6 +37,26 @@ export default Controller.extend({
     });
   }),
 
+  altContacts: computed.map('names', function(c, index) {
+    let [firstName, lastName] = c.split(' ');
+    let [lastInitial] = lastName; // Grab first letter of last name.
+    return {
+      shortName: `${firstName} ${lastInitial}`,
+      emailAddress: `${firstName.toLowerCase()}.${lastInitial.toLowerCase()}@example.com`,
+      profileImage: `http://lorempixel.com/50/50/abstract?${index}`
+    };
+  }),
+
+  selectedAltContacts: computed.filter('altContacts', function(c) {
+    return c.shortName.startsWith('N');
+  }),
+
+  remainingAltContacts: computed('altContacts.[]', 'selectedAltContacts.[]', function() {
+    return this.get('altContacts').filter((c) => {
+      return this.get('selectedAltContacts').indexOf(c) === -1;
+    });
+  }),
+
   vegetables: A([{
     name: 'Broccoli',
     family: 'Brassica'
@@ -118,6 +138,14 @@ export default Controller.extend({
 
     removeContact(item) {
       this.get('selectedContacts').removeObject(item);
+    },
+
+    addAltContact(item) {
+      this.get('selectedAltContacts').pushObject(item);
+    },
+
+    removeAltContact(item) {
+      this.get('selectedAltContacts').removeObject(item);
     }
   }
 });
