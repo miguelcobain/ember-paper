eb786d624c736dbd1b49caedae2909e0a383206b
Paul Nicholls <paul.n@linc-ed.com>
2016-11-04 16:54:11 +1300

    Make paper-contact-chips email, name, image and search fields configurable

--- addon/components/paper-contact-chips.js
+++ addon/components/paper-contact-chips.js
@@ -3,5 +3,9 @@ import PaperChips from 'ember-paper/components/paper-chips';
 export default PaperChips.extend({
   tagName: 'md-contact-chips',
   classNames: ['md-default-theme'],
-  requireMatch: true
+  requireMatch: true,
+  searchField: 'email',
+  emailField: 'email',
+  nameField: 'name',
+  imageField: 'image'
 });
