37ad387a2e39b67a810939d0e1ec019a725f7ebb
Anthony Pessy <anthony.pessy@gmail.com>
2016-12-03 12:57:29 +0100

    add test for paper-tab-label, must have tagName to use elementId

--- addon/components/paper-tab-label.js
+++ addon/components/paper-tab-label.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 const { Component } = Ember;
 
 export default Component.extend({
-  tagName: '',
+
+  tagName: 'span',
+
   didInsertElement() {
     if (this.get('parent')) {
       this.get('parent').send('identifyTabLabel', this.elementId);
