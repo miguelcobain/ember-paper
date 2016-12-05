2f979161611c3b536e8aeb73ebfc59d935a72d62
Anthony Pessy <anthony.pessy@gmail.com>
2016-12-05 09:07:06 +0100

    add some tests for tabs content wrapper

--- addon/components/paper-tabs-content-wrapper.js
+++ addon/components/paper-tabs-content-wrapper.js
@@ -5,8 +5,8 @@ const { Component, run } = Ember;
 export default Component.extend({
   tagName: 'md-tabs-content-wrapper',
 
-  init() {
-    this._super();
+  didInsertElement() {
+    this._super(...arguments);
     run.scheduleOnce('afterRender', () => this.get('parent').send('setWormhole', this.elementId));
   }
 });
