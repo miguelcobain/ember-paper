e10ac0d4251b9fb1c72ff3fe9942f44fc14839d5
Anthony Pessy <anthony.pessy@gmail.com>
2016-12-24 10:50:41 +0100

    add paginated tabs

--- tests/dummy/app/controllers/demo/tabs.js
+++ tests/dummy/app/controllers/demo/tabs.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 const { Controller, String: { camelize } } = Ember;
 
 export default Controller.extend({
+
+  paginatedTabs: Array.from(new Array(10).keys())
+    .map((id) => `Tab ${id}`),
+
   actions: {
     toggleSourceCode(name) {
       this.toggleProperty(camelize(`show-${name}`));
