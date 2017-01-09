8d9925b392fb8bd211872d0f706946eea11ae15c
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2017-01-09 17:43:38 -0200

    Remove unnecessary AM CSS animation

--- addon/components/paper-datepicker-content.js
+++ addon/components/paper-datepicker-content.js
@@ -1,15 +1,6 @@
 import ContentComponent from 'ember-basic-dropdown/components/basic-dropdown/content';
-import { nextTick } from 'ember-css-transitions/mixins/transition-mixin';
 import layout from '../templates/components/paper-datepicker-content';
 
 export default ContentComponent.extend({
-  layout,
-
-  animateIn() {
-    // Add CSS class after one frame to trigger open animation.
-    nextTick().then(() => {
-      let calendarPane = document.getElementById(this.dropdownId);
-      calendarPane.classList.add('md-pane-open');
-    });
-  }
+  layout
 });
