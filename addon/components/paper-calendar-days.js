d3d3d257a79ef0de5921fd2928a5d708c446896d
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2017-01-10 09:03:35 -0200

    Reuse templates from ember-power-calendar

--- addon/components/paper-calendar-days.js
+++ addon/components/paper-calendar-days.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
-import layout from '../templates/components/paper-calendar-days';
 import PowerCalendarDays from 'ember-power-calendar/components/power-calendar/days';
 
 const { computed } = Ember;
 
 export default PowerCalendarDays.extend({
-  layout,
-
-  weekdaysLetters: computed.map('weekdaysNames', function(name) {
+  weekdaysLetters: computed.map('weekdaysMin', function(name) {
     return name.charAt(0);
+  }),
+
+  weekdaysNames: computed('localeStartOfWeek', 'weekdaysLetters', function() {
+    let { localeStartOfWeek, weekdaysLetters } = this.getProperties('localeStartOfWeek', 'weekdaysLetters');
+    return weekdaysLetters.slice(localeStartOfWeek).concat(weekdaysLetters.slice(0, localeStartOfWeek));
   })
 });
--- addon/components/paper-calendar.js
+++ addon/components/paper-calendar.js
@@ -1,9 +1,6 @@
 import PowerCalendar from 'ember-power-calendar/components/power-calendar';
-import layout from '../templates/components/paper-calendar';
 
 export default PowerCalendar.extend({
-  layout,
-
   navComponent: 'paper-calendar-nav',
   daysComponent: 'paper-calendar-days'
 });
