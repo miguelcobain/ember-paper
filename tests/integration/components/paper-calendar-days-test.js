0fbb4f8121cb787372a1fd467739514f89f33606
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2017-01-11 14:01:44 -0200

    Add some tests

--- tests/integration/components/paper-calendar-days-test.js
+++ tests/integration/components/paper-calendar-days-test.js
@@ -1,25 +1,32 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
+import run from 'ember-runloop';
+import getOwner from 'ember-owner/get';
 
+let calendarService, momentService, calendar;
 moduleForComponent('paper-calendar-days', 'Integration | Component | paper calendar days', {
-  integration: true
+  integration: true,
+  beforeEach() {
+    calendarService = getOwner(this).lookup('service:power-calendar-clock');
+    calendarService.set('date', new Date(2013, 9, 18));
+    momentService = getOwner(this).lookup('service:moment');
+    calendar = {
+      center: moment(calendarService.getDate()),
+      locale: 'en',
+      actions: {
+        moveCenter: () => {},
+        select: () => {}
+      }
+    };
+  },
+
+  afterEach() {
+    run(() => momentService.changeLocale('en-US'));
+  }
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{paper-calendar-days}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#paper-calendar-days}}
-      template block text
-    {{/paper-calendar-days}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+test('weekdays contain only one letter', function(assert) {
+  this.render(hbs`{{#paper-calendar as |cal|}}{{cal.days}}{{/paper-calendar}}`);
+  assert.equal(this.$('.ember-power-calendar-weekdays').text().replace(/\s+/g, ' ').trim(), 'S M T W T F S');
 });
