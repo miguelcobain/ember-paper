0625e06ab7db0c1445a07eba3bd691a331bacb16
Anthony Pessy <anthony.pessy@gmail.com>
2017-04-06 10:39:45 +0200

    add exemples in demo app

---
+++ tests/dummy/app/controllers/demo/tabs.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+const { Controller, A, String: { camelize } } = Ember;
+
+export default Controller.extend({
+
+  tabItems: A(Array.from(new Array(10).keys())
+    .map((id) => ({ label: `Tab ${id}`, content: `Content of ${id}` }))),
+
+  stretchTabsOptions: ['auto', 'never', 'always'],
+
+  alignTabsTop: true,
+
+  noInk: false,
+
+  noInkBar: false,
+
+  centerTabs: false,
+
+  itemTwoDisabled: true,
+
+  stretchTabs: 'auto',
+
+  actions: {
+
+    addTabItem(label, content) {
+      this.get('tabItems').pushObject({ label, content });
+    },
+
+    removeTabItem(item) {
+      this.get('tabItems').removeObject(item);
+    },
+
+    toggleSourceCode(name) {
+      this.toggleProperty(camelize(`show-${name}`));
+    }
+  }
+
+});
