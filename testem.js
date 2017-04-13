9859ed504e15ca2dc110e43fbd6fbe8ed6e8fe1c
Björn Harrtell <bjorn@wololo.org>
2017-04-13 10:33:11 +0200

    Upgrade Ember

--- testem.js
+++ testem.js
@@ -1,13 +1,13 @@
-/*jshint node:true*/
+/* eslint-env node */
 module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "PhantomJS"
+  'framework': 'qunit',
+  'test_page': 'tests/index.html?hidepassed',
+  'disable_watching': true,
+  'launch_in_ci': [
+    'PhantomJS'
   ],
-  "launch_in_dev": [
-    "PhantomJS",
-    "Chrome"
+  'launch_in_dev': [
+    'PhantomJS',
+    'Chrome'
   ]
 };
