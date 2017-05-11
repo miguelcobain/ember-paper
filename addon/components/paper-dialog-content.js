3864c01254f5895d01e6fec2ea394d44311ee972
Jouke van der Maas <joukevandermaas@users.noreply.github.com>
2017-05-11 13:18:44 +0200

    Clean up event handlers in paper dialog
    paper-dialog registered event handlers for each image in the dialog, because the size of the content can change when they load. It never cleaned up these handlers, which could cause tests to fail if the dialog in the test contained an image.

--- addon/components/paper-dialog-content.js
+++ addon/components/paper-dialog-content.js
@@ -26,6 +26,16 @@ export default Component.extend({
   didInsertElement() {
     // content overflow might change depending on load of images inside dialog.
     let images = this.$().find('img');
-    images.on('load', run.bind(this, this.imagesLoaded));
+    
+    let imageLoadHandler = run.bind(this, this.imagesLoaded);
+    this.set('imageLoadHandler', imageLoadHandler);
+    
+    images.on('load', imageLoadHandler);
+  },
+  
+  willDestroyElement() {
+    this._super(...arguments);
+    let images = this.$().find('img');
+    images.off('load', this.get('imageLoadHandler'));
   }
 });
