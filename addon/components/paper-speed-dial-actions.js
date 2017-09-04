81c4acd4f2658b895adbde5c91f5ceea1865e6d3
Miguel Andrade <jmandrade0@gmail.com>
2017-09-04 17:25:54 +0100

    add new paper-speed-dial

---
+++ addon/components/paper-speed-dial-actions.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import layout from '../templates/components/paper-speed-dial-actions';
+const { Component } = Ember;
+
+export default Component.extend({
+  layout,
+  tagName: 'md-fab-actions'
+});
---
+++ tests/dummy/app/controllers/demo/speed-dial.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+const { Controller } = Ember;
+
+export default Controller.extend({
+  animation: 'fling',
+  direction: 'down',
+
+  actions: {
+    toggle(propName) {
+      this.toggleProperty(propName);
+    },
+
+    dummy() {
+
+    }
+  }
+});
