c7f3847c62813e7e50647109ef77181357df38b2
Anthony Pessy <anthony@cogniteev.com>
2017-12-27 15:37:04 +0100

    protect paper-menu-content against destroyed component

--- addon/components/paper-select-content.js
+++ addon/components/paper-select-content.js
@@ -36,6 +36,9 @@ export default PaperMenuContent.extend({
   animateIn() {
     run.next(() => {
       run.scheduleOnce('afterRender', this, () => {
+        if (this.isDestroyed || this.isDestroying) {
+          return;
+        }
         let dropdown = this.get('dropdown');
         dropdown.actions.reposition();
         this.set('isActive', true);
