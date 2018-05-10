f90f78ed4174fcbc47ebdda0005ccd4ecf15194f
Miguel Andrade <jmandrade0@gmail.com>
2018-05-10 15:55:29 +0100

    Deployed 3d6f299

--- assets/dummy-fastboot-8af7c10f53bf3cdc4e024e178ab7d5b4.js
+++ assets/dummy-fastboot-8af7c10f53bf3cdc4e024e178ab7d5b4.js
@@ -1,6 +1,6 @@
 define("dummy/initializers/ajax",["exports"],function(e){"use strict"
 Object.defineProperty(e,"__esModule",{value:!0})
-var t=Ember.get,r=function(e){var r=t(this,"fastboot.request.protocol")+":"
+var t=Ember.get,r=function(e){var r=t(this,"fastboot.request.protocol")
 if(/^\/\//.test(e.url))e.url=r+e.url
 else if(!/^https?:\/\//.test(e.url))try{e.url=r+"//"+t(this,"fastboot.request.host")+e.url}catch(e){throw new Error("You are using Ember Data with no host defined in your adapter. This will attempt to use the host of the FastBoot request, which is not configured for the current host of this request. Please set the hostWhitelist property for in your environment.js. FastBoot Error: "+e.message)}if(!najax)throw new Error("najax does not seem to be defined in your app. Did you override it via `addOrOverrideSandboxGlobals` in the fastboot server?")
 najax(e)}
