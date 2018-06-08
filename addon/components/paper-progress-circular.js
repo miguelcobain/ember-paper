666f06d05dc0e90ce4726d1cd34f2daa28b1f542
Miguel Andrade <jmandrade0@gmail.com>
2018-06-08 02:52:55 +0100

    closes #927

--- addon/components/paper-progress-circular.js
+++ addon/components/paper-progress-circular.js
@@ -1,6 +1,7 @@
 /**
  * @module ember-paper
  */
+/* globals FastBoot */
 import { equal } from '@ember/object/computed';
 
 import Component from '@ember/component';
@@ -159,7 +160,7 @@ export default Component.extend(ColorMixin, {
 
   lastAnimationId: 0,
   renderCircle(animateFrom, animateTo, ease = linearEase, animationDuration = 100, iterationCount = 0, dashLimit = 100) {
-    if (this.isDestroyed || this.isDestroying) {
+    if (this.isDestroyed || this.isDestroying || typeof FastBoot !== 'undefined') {
       return;
     }
 
