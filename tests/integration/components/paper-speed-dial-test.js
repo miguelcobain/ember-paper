43031e4df99ce39969447ca1ef82ea555685edb3
Anthony Pessy <anthony@cogniteev.com>
2018-06-11 16:58:31 +0200

    use ember test helpers instead of native dom helpers

--- tests/integration/components/paper-speed-dial-test.js
+++ tests/integration/components/paper-speed-dial-test.js
@@ -1,8 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find, click, triggerEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { find, click, triggerEvent } from 'ember-native-dom-helpers';
 
 module('Integration | Component | paper speed dial', function(hooks) {
   setupRenderingTest(hooks);
--- tests/integration/components/paper-tabs-test.js
+++ tests/integration/components/paper-tabs-test.js
@@ -1,8 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find, click, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import { find, click, findAll } from 'ember-native-dom-helpers';
 
 module('Integration | Component | paper tabs', function(hooks) {
   setupRenderingTest(hooks);
