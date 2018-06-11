4f85cde21d60f539f6d3aea315eed73651f6a96d
Anthony Pessy <anthony.pessy@gmail.com>
2018-06-11 18:00:26 +0200

    paper-progress-circular:  do not rely ember rAF/cAF from ember-css-transitions (#945)
    * update paper-progress-circular tests
    
    * do not rely ember raf/caf from ember css transitions.
    
    It prevents the rendering from completing during tests.

--- tests/integration/components/paper-progress-circular-test.js
+++ tests/integration/components/paper-progress-circular-test.js
@@ -1,45 +1,46 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import wait from 'ember-test-helpers/wait';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('paper-progress-circular', 'Integration | Component | paper progress circular', {
-  integration: true
-});
+module('Integration | Component | paper progress circular', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('should auto-set the md-mode to "indeterminate" if not specified', function(assert) {
-  assert.expect(1);
-  this.render(hbs`{{paper-progress-circular}}`);
-  assert.equal(this.$('md-progress-circular').attr('md-mode').trim(), 'indeterminate');
-});
+  test('should auto-set the md-mode to "indeterminate" if not specified', async function(assert) {
+    assert.expect(1);
+    await render(hbs`{{paper-progress-circular}}`);
+    assert.equal(this.$('md-progress-circular').attr('md-mode').trim(), 'indeterminate');
+  });
 
-test('should auto-set the md-mode to "determinate" if a value is specified', function(assert) {
-  assert.expect(1);
-  this.render(hbs`{{paper-progress-circular value=12}}`);
-  assert.equal(this.$('md-progress-circular').attr('md-mode').trim(), 'determinate');
-});
+  test('should auto-set the md-mode to "determinate" if a value is specified', async function(assert) {
+    assert.expect(1);
+    await render(hbs`{{paper-progress-circular value=12}}`);
+    assert.equal(this.$('md-progress-circular').attr('md-mode').trim(), 'determinate');
+  });
 
-test('should set correct size based on diameter', function(assert) {
-  assert.expect(2);
-  this.render(hbs`{{paper-progress-circular diameter=25}}`);
+  test('should set correct size based on diameter', async function(assert) {
+    assert.expect(2);
+    await render(hbs`{{paper-progress-circular diameter=25}}`);
 
-  let $el = this.$('md-progress-circular');
-  assert.ok(/height:.*25px/.test($el.attr('style')));
-  assert.ok(/width:.*25px/.test($el.attr('style')));
-});
+    let $el = this.$('md-progress-circular');
+    assert.ok(/height:.*25px/.test($el.attr('style')));
+    assert.ok(/width:.*25px/.test($el.attr('style')));
+  });
 
-test('renders correctly with explicit value and diameter', async function(assert) {
-  assert.expect(5);
+  test('renders correctly with explicit value and diameter', async function(assert) {
+    assert.expect(5);
 
-  this.render(hbs`{{paper-progress-circular value=50 diameter=25}}`);
+    await render(hbs`{{paper-progress-circular value=50 diameter=25}}`);
 
-  await wait();
+    await settled();
 
-  let $el = this.$('md-progress-circular');
-  assert.ok(/height:.*25px/.test($el.attr('style')));
-  assert.ok(/width:.*25px/.test($el.attr('style')));
+    let $el = this.$('md-progress-circular');
+    assert.ok(/height:.*25px/.test($el.attr('style')));
+    assert.ok(/width:.*25px/.test($el.attr('style')));
 
-  let $svgPath = $el.find('path');
-  assert.equal('rotate(0 12.5 12.5)', $svgPath.attr('transform'), 'rotated halfway');
-  assert.ok(parseFloat($svgPath.attr('stroke-dashoffset')), 'stroke-dashoffset has a number');
-  assert.ok(parseFloat($svgPath.attr('stroke-dasharray')), 'stroke-dasharray has a number');
+    let $svgPath = $el.find('path');
+    assert.equal('rotate(0 12.5 12.5)', $svgPath.attr('transform'), 'rotated halfway');
+    assert.ok(parseFloat($svgPath.attr('stroke-dashoffset')), 'stroke-dashoffset has a number');
+    assert.ok(parseFloat($svgPath.attr('stroke-dasharray')), 'stroke-dasharray has a number');
+  });
 });
