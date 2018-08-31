b3472c7e974df80a0c027cd5c90d814d7d3fcffc
Alberto Cantú Gómez <betocantu93@gmail.com>
2018-08-31 13:09:25 -0500

    paper-radio-base implementation of safeClosureAction

--- addon/components/paper-radio-base.js
+++ addon/components/paper-radio-base.js
@@ -9,6 +9,7 @@ import layout from '../templates/components/paper-radio-base';
 import FocusableMixin from 'ember-paper/mixins/focusable-mixin';
 import RippleMixin from 'ember-paper/mixins/ripple-mixin';
 import ColorMixin from 'ember-paper/mixins/color-mixin';
+import { safeClosureAction } from '../utils/actions';
 
 /**
  * @class PaperRadio
@@ -49,9 +50,9 @@ export default Component.extend(FocusableMixin, RippleMixin, ColorMixin, {
   click() {
     if (!this.get('disabled')) {
       if (this.get('toggle')) {
-        this.sendAction('onChange', this.get('checked') ? null : this.get('value'));
+        safeClosureAction(this, 'onChange', this.get('checked') ? null : this.get('value'));
       } else {
-        this.sendAction('onChange', this.get('value'));
+        safeClosureAction(this, 'onChange', this.get('value'));
       }
     }
     // Prevent bubbling, if specified. If undefined, the event will bubble.
