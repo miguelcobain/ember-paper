5f272659b9d39f98a5e94d1adf72703de9acc38a
Beto Cantu <betocantu93@gmail.com>
2018-11-01 04:14:19 -0600

    refactor jquery from checkbox tests

--- tests/integration/components/paper-checkbox-test.js
+++ tests/integration/components/paper-checkbox-test.js
@@ -1,8 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, triggerKeyEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import $ from 'jquery';
 
 module('Integration | Component | paper checkbox', function(hooks) {
   setupRenderingTest(hooks);
@@ -12,10 +11,12 @@ module('Integration | Component | paper checkbox', function(hooks) {
 
     this.set('value', true);
     await render(hbs`{{paper-checkbox value=value label="Blue" onChange=(action (mut value))}}`);
-    assert.ok(this.$('md-checkbox').hasClass('md-checked'));
+    
+    assert.dom('md-checkbox').hasClass('md-checked');
 
     this.set('value', false);
-    assert.ok(!this.$('md-checkbox').hasClass('md-checked'));
+    assert.dom('md-checkbox').doesNotHaveClass('md-checked');
+    
   });
 
   test('should trigger an action when unchecking', async function(assert) {
@@ -28,7 +29,7 @@ module('Integration | Component | paper checkbox', function(hooks) {
 
     await render(hbs`{{paper-checkbox value=value onChange=handleChange}}`);
 
-    this.$('md-checkbox').click();
+    await click('md-checkbox');
   });
 
   test('should trigger an action when checking', async function(assert) {
@@ -41,7 +42,8 @@ module('Integration | Component | paper checkbox', function(hooks) {
 
     await render(hbs`{{paper-checkbox value=value onChange=handleChange}}`);
 
-    this.$('md-checkbox').click();
+    await click('md-checkbox');
+    
   });
 
   test('shouldn\'t trigger an action when disabled', async function(assert) {
@@ -54,7 +56,7 @@ module('Integration | Component | paper checkbox', function(hooks) {
 
     await render(hbs`{{paper-checkbox disabled=true value=value onChange=handleChange}}`);
 
-    this.$('md-checkbox').click();
+    await click('md-checkbox');
   });
 
   // space and enter key codes
@@ -66,10 +68,9 @@ module('Integration | Component | paper checkbox', function(hooks) {
       await render(hbs`{{paper-checkbox value=value onChange=(action (mut value))}}`);
       assert.equal(this.get('value'), false);
 
-      let e = new $.Event('keypress');
-      e.which = keyCode; // # Some key code value
-      this.$('md-checkbox').trigger(e);
-
+      
+      await triggerKeyEvent('md-checkbox', 'keypress', keyCode);
+      
       assert.equal(this.get('value'), true);
     });
 
@@ -80,9 +81,7 @@ module('Integration | Component | paper checkbox', function(hooks) {
       await render(hbs`{{paper-checkbox value=value onChange=(action (mut value))}}`);
       assert.equal(this.get('value'), true);
 
-      let e = new $.Event('keypress');
-      e.which = keyCode; // # Some key code value
-      this.$('md-checkbox').trigger(e);
+      await triggerKeyEvent('md-checkbox', 'keypress', keyCode);
 
       assert.equal(this.get('value'), false);
     });
@@ -93,7 +92,7 @@ module('Integration | Component | paper checkbox', function(hooks) {
 
     await render(hbs`{{paper-checkbox value=value onChange=(action (mut value)) label="çup?"}}`);
 
-    assert.equal(this.$('.md-label > span').text().trim(), 'çup?');
+    assert.dom('.md-label > span').hasText('çup?');
   });
 
   test('block version should set label inside', async function(assert) {
@@ -105,7 +104,7 @@ module('Integration | Component | paper checkbox', function(hooks) {
       {{/paper-checkbox}}
     `);
 
-    assert.equal(this.$('.md-label > span').text().trim(), 'çup?');
+    assert.dom('.md-label > span').hasText('çup?');
   });
 
   /* test('the `onChange` action is mandatory', function(assert) {
@@ -124,10 +123,11 @@ module('Integration | Component | paper checkbox', function(hooks) {
       {{paper-checkbox value=value indeterminate=indeterminate
         label="Blue" onChange=(action (mut value))}}
     `);
-    assert.ok(this.$('md-checkbox').hasClass('md-checked'));
+
+    assert.dom('md-checkbox').hasClass('md-checked');
 
     this.set('indeterminate', true);
-    assert.ok(!this.$('md-checkbox').hasClass('md-checked'));
-    assert.ok(this.$('md-checkbox').hasClass('md-indeterminate'));
+    assert.dom('md-checkbox').doesNotHaveClass('md-checked');
+    assert.dom('md-checkbox').hasClass('md-indeterminate');
   });
 });
