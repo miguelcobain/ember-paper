8163c3188a728eab5b4fcbb0c0dcc8a11d6087d5
Beto Cantu <betocantu93@gmail.com>
2018-11-01 16:17:07 -0600

    refactor jquery from paper-switch

--- addon/components/paper-switch.js
+++ addon/components/paper-switch.js
@@ -85,9 +85,9 @@ export default Component.extend(FocusableMixin, RippleMixin, ColorMixin, Proxiab
   },
 
   _setupSwitch() {
-    this.set('switchWidth', this.$('.md-thumb-container').innerWidth());
+    this.set('switchWidth', this.element.querySelector('.md-thumb-container').innerWidth);
 
-    let switchContainer = this.$('.md-container').get(0);
+    let switchContainer = this.element.querySelector('.md-container');
     let switchHammer = new Hammer(switchContainer);
     this._switchContainerHammer = switchHammer;
 
@@ -100,7 +100,12 @@ export default Component.extend(FocusableMixin, RippleMixin, ColorMixin, Proxiab
     // Enable tapping gesture on the switch
     this._switchHammer = new Hammer(this.element);
     this._switchHammer.on('tap', run.bind(this, this._dragEnd));
-    this.$('.md-container').on('click', run.bind(this, this._handleNativeClick));
+
+    this._onClickHandleNativeClick = run.bind(this, this._handleNativeClick);
+
+    this.element.querySelector('.md-container')
+      .addEventListener('click', this._onClickHandleNativeClick)
+    
   },
 
   _handleNativeClick() {
