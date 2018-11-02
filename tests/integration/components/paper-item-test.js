7e39ad24675c95445c0169a8fb3453efd08dab4c
Beto Cantu <betocantu93@gmail.com>
2018-11-02 04:29:14 -0600

    fix eslint

--- tests/integration/components/paper-item-test.js
+++ tests/integration/components/paper-item-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, settled, click, findAll, find } from '@ember/test-helpers';
+import { render, settled, click, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | paper item', function(hooks) {
