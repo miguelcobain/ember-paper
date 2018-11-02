56b6662db32e827bfc39e794f91ccc4a133470dd
Alberto Cantú Gómez <betocantu93@hotmail.com>
2018-11-02 06:33:46 -0600

    refactor jquery from tests (#1015)

--- tests/integration/components/paper-progress-linear-test.js
+++ tests/integration/components/paper-progress-linear-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | paper progress linear', function(hooks) {
@@ -8,23 +8,23 @@ module('Integration | Component | paper progress linear', function(hooks) {
 
   test('should auto-set the md-mode to "indeterminate" if not specified', async function(assert) {
     await render(hbs`{{paper-progress-linear}}`);
-    assert.equal(this.$('md-progress-linear').attr('md-mode').trim(), 'indeterminate');
+    assert.dom('md-progress-linear').hasAttribute('md-mode', 'indeterminate');
   });
 
   test('should auto-set the md-mode to "determinate" if a value is specified', async function(assert) {
     await render(hbs`{{paper-progress-linear value=12}}`);
-    assert.equal(this.$('md-progress-linear').attr('md-mode').trim(), 'determinate');
+    assert.dom('md-progress-linear').hasAttribute('md-mode', 'determinate');
   });
 
   test('should auto-set the md-mode to "buffer" if a value and bufferValue is specified', async function(assert) {
     await render(hbs`{{paper-progress-linear  value=50  bufferValue=100}}`);
-    assert.equal(this.$('md-progress-linear').attr('md-mode'), 'buffer');
+    assert.dom('md-progress-linear').hasAttribute('md-mode', 'buffer');
   });
 
   test('it sets transform based on value', async function(assert) {
     await render(hbs`{{paper-progress-linear value=50}}`);
 
-    let bar2 = this.$('.md-bar2').get(0);
+    let bar2 = find('.md-bar2');
     let bar2style = bar2.style.transform || bar2.style['-webkit-transform'];
 
     assert.equal(bar2style, 'translateX(-25%) scale(0.5, 1)', 'Transition set correctly');
@@ -33,7 +33,7 @@ module('Integration | Component | paper progress linear', function(hooks) {
   test('it sets transform based on buffer value', async function(assert) {
     await render(hbs`{{paper-progress-linear value=50 bufferValue=75}}`);
 
-    let bar1 = this.$('.md-bar1').get(0);
+    let bar1 = find('.md-bar1');
     let bar1style = bar1.style.transform || bar1.style['-webkit-transform'];
 
     assert.equal(bar1style, 'translateX(-12.5%) scale(0.75, 1)', 'Buffer bar transition set correctly');
@@ -42,7 +42,7 @@ module('Integration | Component | paper progress linear', function(hooks) {
   test('it should not set transition in query mode', async function(assert) {
     await render(hbs`{{paper-progress-linear value=80 mode="query"}}`);
 
-    let bar2 = this.$('.md-bar2').get(0);
+    let bar2 = find('.md-bar2');
     let bar2style = bar2.style.transform || bar2.style['-webkit-transform'];
 
     assert.ok(!bar2style, 'Buffer bar not set');
