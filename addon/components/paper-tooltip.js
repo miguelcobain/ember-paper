f2c33274fb188be254ad13fcbc33cc26202012f6
blamens <bob.lamens@gmail.com>
2019-01-23 14:16:06 +0100

    Added component API to set a scroll container for paper-tooltip event handlers

--- addon/components/paper-tooltip.js
+++ addon/components/paper-tooltip.js
@@ -100,7 +100,9 @@ export default Component.extend({
       enterEventHandler,
       supportsPassiveEventListeners ? { passive: true } : false);
 
-    window.addEventListener('scroll', leaveHandler);
+    let scrollContainer = this.get('scrollContainer');
+
+    scrollContainer ? scrollContainer.addEventListener('scroll', leaveHandler) : window.addEventListener('scroll', leaveHandler);
     window.addEventListener('blur', leaveHandler);
     window.addEventListener('resize', leaveHandler);
     window.addEventListener('orientationchange', leaveHandler);
@@ -109,7 +111,10 @@ export default Component.extend({
 
   willDestroyElement() {
     this._super(...arguments);
-    window.removeEventListener('scroll', this.leaveHandler);
+
+    let scrollContainer = this.get('scrollContainer');
+
+    scrollContainer ? scrollContainer.removeEventListener('scroll', this.leaveHandler) : window.removeEventListener('scroll', this.leaveHandler);
     window.removeEventListener('blur', this.leaveHandler);
     window.removeEventListener('resize', this.leaveHandler);
     window.removeEventListener('orientationchange', this.leaveHandler);
