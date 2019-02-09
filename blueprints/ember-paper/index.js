bb85615393d8d8bfc10344ee82e6bdaa75409361
Beto Cantu <betocantu93@gmail.com>
2019-02-09 16:41:35 -0600

    refactor to fileMapTokens

--- blueprints/ember-paper/index.js
+++ blueprints/ember-paper/index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const rsvp = require('rsvp');
-const fs = require('fs-extra');
-const path = require('path');
-const writeFile = rsvp.denodeify(fs.writeFile);
 const isModuleUnificationProject = require('../module-unification').isModuleUnificationProject;
 
 module.exports = {
@@ -14,28 +10,24 @@ module.exports = {
     // not specified (since that doesn't actually matter
     // to us
   },
-  addAppScss() {
-    let importStatement = '\n@import "ember-paper";\n';
 
-    let stylePath = path.join('app', 'styles');
-
+  fileMapTokens() {
     if (isModuleUnificationProject(this.project)) {
-      stylePath = path.join('src', 'ui', 'styles');
-    }
-
-    let file = path.join(stylePath, 'app.scss');
-
-    if (!fs.existsSync(stylePath)) {
-      fs.mkdirSync(stylePath);
-    }
-    if (fs.existsSync(file)) {
-      return this.insertIntoFile(file, importStatement, {});
+      return {
+        __path__(options) {
+          return 'src/ui';
+        }
+      };
     } else {
-      return writeFile(file, importStatement);
+      return {
+        __path__(options) {
+          return 'app';
+        }
+      };
     }
   },
 
   afterInstall() {
-    return this.addAddonToProject('ember-cli-sass', '^7.2.0').then(() => this.addAppScss());
+    return this.addAddonToProject('ember-cli-sass', '^7.2.0');
   }
 };
