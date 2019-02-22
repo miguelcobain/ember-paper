395ec7bdc61482ee7d031354ce0b5e389f9bc309
Alberto Cantú Gómez <betocantu93@hotmail.com>
2019-02-22 11:01:58 -0600

    module-unification bluerpint (#1064)
    * use fileMapTokens, add module-unification file, scss blueprint and update resolver
    
    * exclude blueprints/module-unification.js from eslint
    
    * add eslintrc.js config to blueprints
    
    * remove blueprints/module-unification from excluded

---
+++ blueprints/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  env: {
+    node: true
+  }
+};
---
+++ blueprints/module-unification.js
@@ -0,0 +1,8 @@
+
+'use strict';
+
+module.exports = {
+  isModuleUnificationProject(project) {
+    return project && project.isModuleUnification && project.isModuleUnification();
+  }
+};
