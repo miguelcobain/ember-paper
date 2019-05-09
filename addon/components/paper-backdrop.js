b2f4f2e1950158dad541ba658cf083f12b7b0ae0
Buck Doyle <b@chromatin.ca>
2019-05-09 18:54:20 -0500

    Add guard against missing original.parentNode
    This prevents the test failure seen here:
    https://travis-ci.org/backspace/prison-rideshare-ui/builds/530523899#L578

--- addon/components/paper-backdrop.js
+++ addon/components/paper-backdrop.js
@@ -29,7 +29,9 @@ export default Component.extend(TransitionMixin, {
   }),
 
   addDestroyedElementClone(original, clone) {
-    original.parentNode.appendChild(clone);
+    if (original.parentNode) {
+      original.parentNode.appendChild(clone);
+    }
   },
 
   sendClickAction(e) {
