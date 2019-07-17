deee8374e571f152444e32106e68813876670e0a
betocantu93 <betocantu93@gmail.com>
2019-07-17 17:44:02 -0500

    documentation

---
+++ tests/dummy/snippets/errorMessages.js
@@ -0,0 +1,9 @@
+ENV['ember-paper'] = {
+  errorMessages: {
+    max: 'Must be less than %@.',
+    maxlength: 'Must not exceed %@ characters.',
+    min: 'Must be at least %@.',
+    minlength: 'Must have at least %@ characters.',
+    required: 'This is required.'
+  }
+};
