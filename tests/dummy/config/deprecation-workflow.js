89e9eab49d9b3187980cb199ebdda0905d3d955f
Chris Manson <chris@manson.ie>
2024-10-22 11:06:55 +0100

    make sure ember-cli-deprecation-workflow config is up to date

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -2,12 +2,16 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
+    { handler: "throw", matchId: "deprecated-run-loop-and-computed-dot-access" },
     { handler: "silence", matchId: "ember-global" },
-    { handler: "silence", matchId: "this-property-fallback" },
-    { handler: "silence", matchId: "ensure-safe-component.string" },
-    { handler: "silence", matchId: "deprecated-run-loop-and-computed-dot-access" },
     { handler: "silence", matchId: "ember-metal.get-with-default" },
+    { handler: "silence", matchId: "ember-modifier.no-args-property" },
+    { handler: "silence", matchId: "ember-modifier.no-element-property" },
+    { handler: "silence", matchId: "ember-modifier.use-destroyables" },
+    { handler: "silence", matchId: "ember-modifier.use-modify" },
+    { handler: "silence", matchId: "ember-string.loc" },
     { handler: "silence", matchId: "ember-utils.try-invoke" },
-    { handler: "silence", matchId: "ember-string.loc" }
+    { handler: "silence", matchId: "ensure-safe-component.string" },
+    { handler: "silence", matchId: "this-property-fallback" },
   ]
 };
