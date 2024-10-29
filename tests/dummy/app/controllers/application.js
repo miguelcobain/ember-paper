0882cc1e60c320f8d82b2cbf3ad8ad3c205a887d
Matthew Hartstonge <matt@mykro.co.nz>
2024-10-30 11:14:38 +1300

    fix(tests/dummy/app): removes `application-controller.router-properties` deprecations.

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,9 +1,12 @@
-/* eslint-disable ember/no-actions-hash, prettier/prettier */
+/* eslint-disable ember/no-actions-hash */
 import { equal } from '@ember/object/computed';
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  router: service(),
+
   actions: {
     toggleExpandedItem(value, ev) {
       if (this.expandedItem === value) {
@@ -11,11 +14,11 @@ export default Controller.extend({
       }
       this.set('expandedItem', value);
       ev.stopPropagation();
-    }
+    },
   },
 
-  expandedItem: computed('currentRouteName', function() {
-    if (this.currentRouteName.substr(0, 6) === 'layout') {
+  expandedItem: computed('router.currentRouteName', function () {
+    if (this.router.currentRouteName.substr(0, 6) === 'layout') {
       return 'layout';
     } else {
       return 'demos';
@@ -23,5 +26,5 @@ export default Controller.extend({
   }),
 
   demosExpanded: equal('expandedItem', 'demos'),
-  layoutExpanded: equal('expandedItem', 'layout')
+  layoutExpanded: equal('expandedItem', 'layout'),
 });
