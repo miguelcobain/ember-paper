b5e8a70fc44e1b30fffa8bc61f87517b241d7c06
Matthew Hartstonge <matt@mykro.co.nz>
2024-12-12 15:34:30 +1300

    feat(addon/components/paper-autocomplete::ebd-content): converts to a glimmer component.

--- app/components/paper-autocomplete.js
+++ app/components/paper-autocomplete.js
@@ -1,1 +1,1 @@
-export { default } from 'ember-paper/components/paper-autocomplete/component';
+export { default } from 'ember-paper/components/paper-autocomplete';
--- app/components/paper-autocomplete/eps-trigger.js
+++ app/components/paper-autocomplete/eps-trigger.js
@@ -1,1 +1,1 @@
-export { default } from 'ember-paper/components/paper-autocomplete/eps-trigger/component';
+export { default } from 'ember-paper/components/paper-autocomplete/eps-trigger';
